869a1fd2760a03c4daa771fcfd221e6f8b6c593d
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-22 13:54:19 +0000

    Add Rates DB Class
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@11 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/rates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lync_Billing.DB
+{
+    public class rates
+    {
+        public string RateTableName { get; set; }
+        public string FixedlineRate { get; set; }
+        public string MobileLineRate { get; set; }
+    }
+}
