8569c7e04e3f9fcb2de74b3f607d76b78838779f
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-23 07:29:36 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@15 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Users.cs
+++ Lync-Billing/DB/Users.cs
@@ -10,9 +10,32 @@ namespace Lync_Billing.DB
         
         public string employeeID { get; set; }
         public string userName { get; set; }
+        public string UPN { get; set; }
         public List<int> PoolIDs { get; set; }
         public List<int> GatewayIDs { get; set; }
         public string RoleID { get; set; }
+
+        public int INSERT() 
+        {
+            return 0;
+        }
+
+        public List<Users> SELECT() 
+        {
+            List<Users> users = new List<Users>();
+
+            return users;
+        }
+
+        public void UPDATE()
+        {
+
+        }
+
+        public void DELETE() 
+        {
+
+        }
 
     }
 }
