12d2d546e748fe6620ba5c63c34cc63ed2068842
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-23 07:30:19 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@16 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/rates.cs
+++ Lync-Billing/DB/rates.cs
@@ -5,10 +5,34 @@ using System.Web;
 
 namespace Lync_Billing.DB
 {
-    public class rates
+    public class Rates
     {
-        public string RateTableName { get; set; }
+        public string GatewayName { get; set; }
         public string FixedlineRate { get; set; }
         public string MobileLineRate { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndingDate { get; set; }
+
+        public int INSERT()
+        {
+            return 0;
+        }
+
+        public List<Rates> SELECT()
+        {
+            List<Rates> rates = new List<Rates>();
+
+            return rates;
+        }
+
+        public void UPDATE()
+        {
+
+        }
+
+        public void DELETE()
+        {
+
+        }
     }
 }
