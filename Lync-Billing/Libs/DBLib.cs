c0e1e896d44ff068406d2d61e55b2db7139f3a64
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-23 09:33:44 +0000

    Add the code for INSERT UPDATE DELETE SELECT
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@17 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/Libs/DBLib.cs
+++ Lync-Billing/Libs/DBLib.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Data.OleDb;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 
 
 namespace Lync_Billing.Libs
@@ -12,33 +13,135 @@ namespace Lync_Billing.Libs
     
     public class DBLib
     {
-        //public static string ConnectionString_Lync = @"Provider=SQLOLEDB.1;Data Source=10.1.60.55;Persist Security Info=True;Password='=25_ar;p1100';User ID=sa;Initial Catalog=LyncBilling";
+        public static string ConnectionString_Lync = @"Provider=SQLOLEDB.1;Data Source=10.1.60.55;Persist Security Info=True;Password='=25_ar;p1100';User ID=sa;Initial Catalog=LyncBilling";
 
         private OleDbConnection DBInitializeConnection(string connectionString) 
         {
             return new OleDbConnection(connectionString);
         }
 
-        public OleDbDataReader selectFrom(string tableName,Dictionary<string,object> columns,string whereStatemnet)
+        public DataTable selectFrom(string tableName,List<string> columns,string whereStatemnet)
         {
-            OleDbDataReader dr = new OleDbDataReader();
-            //string inserQuery = string.Format("select ")
-            return dr;
+            DataTable dt = new DataTable();
+            OleDbDataReader dr;
+            
+            StringBuilder constructedFields = new StringBuilder();
+            
+            constructedFields.Append("(");
+
+            foreach(string field in columns)
+            {
+                constructedFields.Append(field+",");
+
+            }
+            
+            constructedFields.Remove(constructedFields.Length-1,1);
+            constructedFields.Append(")");
+
+            string selectQuery = string.Format("select from '{2}' where '{3}'", tableName, constructedFields.ToString(), whereStatemnet);
+
+            OleDbConnection conn = DBInitializeConnection(ConnectionString_Lync);
+            OleDbCommand comm = new OleDbCommand(selectQuery, conn);
+
+            try
+            {
+                conn.Open();
+                dr = comm.ExecuteReader();
+                dt.Load(dr);
+            }
+            catch (Exception ex) { throw ex; }
+            finally { conn.Close(); }
+
+            return dt;
         }
 
-        public int insertTo(string tableName, Dictionary<string, object> columnValue) 
+        public int INSERT(string tableName, Dictionary<string, object> columnsValues, string whereStatement) 
         {
-            return 0;
+            StringBuilder fields = new StringBuilder(); fields.Append("(");
+            StringBuilder values = new StringBuilder(); values.Append("(");
+
+            foreach (KeyValuePair<string, object> pair in columnsValues)
+            {
+                fields.Append(pair.Key + ",");
+
+                Type valueType = pair.Value.GetType();
+
+                if (valueType == typeof(int) || valueType == typeof(Double))
+                    values.Append(pair.Value + ",");
+                else
+                    values.Append("'" + pair.Value + "'" + ",");
+            }
+
+            fields.Remove(fields.Length - 1, 1).Append(")");
+            values.Remove(values.Length - 1, 1).Append(")");
+
+            string insertQuery = string.Format("INSERT INTO '{1}' '{2}' VALUES '{3}' WHERE '{4}'; SELECT SCOPE_IDENTITY();", tableName, fields, values, whereStatement);
+
+            OleDbConnection conn = DBInitializeConnection(ConnectionString_Lync);
+            OleDbCommand comm = new OleDbCommand(insertQuery, conn);
+
+            int recordID = 0;
+            try
+            {
+                conn.Open();
+                recordID = (int)comm.ExecuteScalar();
+            }
+            catch (Exception ex) { throw ex; }
+            finally { conn.Close(); }
+
+            return recordID;
         }
 
-        public int update(string tablename, Dictionary<string, object> columnValue, string whereStatement)
+        public bool UPDATE(string tableName, Dictionary<string, object> columnsValues, string whereStatement)
         {
-            return 0;
+
+            StringBuilder fieldsValues = new StringBuilder();
+            
+
+            foreach (KeyValuePair<string, object> pair in columnsValues)
+            {
+
+                Type valueType = pair.Value.GetType();
+
+                if (valueType == typeof(int) || valueType == typeof(Double))
+                    fieldsValues.Append(pair.Key + "=" + pair.Value + ",");              
+                else
+                    fieldsValues.Append(pair.Key + "=" + "'" + pair.Value  + "'" + ",");  
+            }
+            
+            fieldsValues.Remove(fieldsValues.Length - 1, 1).Append(")");
+
+            string insertQuery = string.Format("UPDATE  '{1}' SET '{2}' WHERE '{3}'", tableName, fieldsValues, whereStatement);
+
+            OleDbConnection conn = DBInitializeConnection(ConnectionString_Lync);
+            OleDbCommand comm = new OleDbCommand(insertQuery, conn);
+
+            try
+            {
+                conn.Open();
+                comm.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex) { throw ex; }
+            finally { conn.Close(); }
+
         }
 
-        public bool deleteFrom(string tableName, string whereStatement) 
+        public bool DELETE(string tableName, string whereStatement) 
         {
-            return false;
+            string deleteQuery = string.Format("DELETE FROM '{1}' WHERE '{2}'", tableName, whereStatement);
+
+            OleDbConnection conn = DBInitializeConnection(ConnectionString_Lync);
+            OleDbCommand comm = new OleDbCommand(deleteQuery, conn);
+
+            try
+            {
+                conn.Open();
+                comm.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex) { throw ex; }
+            finally { conn.Close(); }
         }
 
    }
