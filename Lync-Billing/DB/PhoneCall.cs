83d6a8ea38ed3eb6f184d98a70c5b10da32d4a25
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-24 05:32:19 +0000

    Add SELECT Methods
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@23 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/PhoneCall.cs
+++ Lync-Billing/DB/PhoneCall.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Lync_Billing.Libs;
+using System.Data;
 
 namespace Lync_Billing.DB
 {
     public class PhoneCall
     {
-        public int PhoneCallID { set; get; }
+        public DBLib DBRoutines = new DBLib();
+       
+        public long PhoneCallID { set; get; }
         public string SrcNumber { set; get; }
         public string DstNumber { set; get; }
         public string SipAccount { set; get; }
@@ -18,38 +22,95 @@ namespace Lync_Billing.DB
         public string Gateway { set; get; }
 
         public double Duration { set; get; }
-        public double rate { set; get; }
+        public double Rate { set; get; }
         public double Cost { set; get; }
         
         public DateTime UpdateOn{ set; get; }
         public DateTime ModifiedOn { set; get; }
-        public DateTime CalledOn { set; get; }
+        public DateTime DateOfCall { set; get; }
         
+
         public bool IsPersonal { set; get; }
         public bool Dispute { set; get; }
         public bool Payed { set; get; }
         public bool BillIt { set; get; }
 
-        public int INSERT()
-        {
-            return 0;
-        }
 
-        public List<PhoneCall> SELECT()
+        public List<PhoneCall> GetPhoneCalls(List<string> columns,Dictionary<string,object> wherePart,bool allRows,int limits)
         {
+            DataTable dt = new DataTable();
             List<PhoneCall> phoneCalls = new List<PhoneCall>();
+            
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.PhoneCalls.TableName), wherePart, limits, allRows);
+           
+            foreach(DataRow row in dt.Rows)
+            {
+                PhoneCall phoneCall = new PhoneCall();
+               
+                foreach(DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.BillIt))
+                        phoneCall.BillIt = (bool)row[column.ColumnName];
 
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.Cost))
+                        phoneCall.Cost = (double)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.DateOfCall))
+                        phoneCall.DateOfCall = (DateTime)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.Dispute))
+                        phoneCall.Dispute = (bool)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.DstCountry))
+                        phoneCall.DstCountry = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.DstNumber))
+                        phoneCall.DstNumber = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.Duration))
+                        phoneCall.Duration = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.Gateway))
+                        phoneCall.Gateway = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.IsPersonal))
+                        phoneCall.IsPersonal = (bool)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.ModifiedBy))
+                        phoneCall.ModifiedBy = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.ModifiedOn))
+                        phoneCall.ModifiedOn = (DateTime)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.Payed))
+                        phoneCall.Payed = (bool)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.PhoneCallID))
+                        phoneCall.PhoneCallID = (long)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.Rate))
+                        phoneCall.Rate = (double)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.SipAccount))
+                        phoneCall.SipAccount = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.SrcNumber))
+                        phoneCall.SrcNumber = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.TypeOfService))
+                        phoneCall.TypeOfService = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.UpdatedBy))
+                        phoneCall.UpdatedBy = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneCalls.UpdateOn))
+                        phoneCall.UpdateOn = (DateTime)row[column.ColumnName];
+                    
+                }
+                phoneCalls.Add(phoneCall);
+            }
             return phoneCalls;
         }
 
-        public void UPDATE()
-        {
-
-        }
-
-        public void DELETE()
-        {
-
-        }
     }
 }
