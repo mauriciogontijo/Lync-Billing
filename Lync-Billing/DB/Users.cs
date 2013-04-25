fbb075a3c95968baf8625b6f197d290800ae74c1
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-25 12:56:45 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@55 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Users.cs
+++ Lync-Billing/DB/Users.cs
@@ -8,41 +8,36 @@ namespace Lync_Billing.DB
     public class Users
     {
         
-        public string employeeID { get; set; }
-        public string userName { get; set; }
+        public string EmployeeID { get; set; }
+        public string LoginName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
         public string UPN { get; set; }
         public List<int> PoolIDs { get; set; }
         public List<int> GatewayIDs { get; set; }
         public string RoleID { get; set; }
 
-        private int GetUsers() 
-        {
-
-            return 0;
-        }
-
-        private List<Users> InsertUsers() 
+        private List<Users> GetUsers(List<string> columns, Dictionary<string, object> wherePart, bool allFields, int limits) 
         {
             List<Users> users = new List<Users>();
 
             return users;
+            
         }
 
-        private bool UpdateUsers()
+        private int InsertUsers(List<Users> users) 
+        {
+            
+
+            return 0;
+        }
+
+        private bool UpdateUsers(List<Users> users)
         {
             bool status = false;
 
 
             return status;
         }
-
-        private bool DeleteUsers() 
-        {
-            bool status = false;
-
-
-            return status;
-        }
-
     }
 }
