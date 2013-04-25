2622838528c324e0441f70dccaa3809bad38b818
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-25 06:07:36 +0000

    Add Select Method
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@35 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Pools.cs
+++ Lync-Billing/DB/Pools.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using Lync_Billing.Libs;
 
 namespace Lync_Billing.DB
 {
     public class Pools
     {
+        public DBLib DBRoutines = new DBLib();
         public int PoolID { set; get; }
         public string PoolName { set; get; }
 
@@ -15,10 +18,27 @@ namespace Lync_Billing.DB
             return 0;
         }
 
-        public List<Pools> SELECT()
+        public List<Pools> GetPools(List<string> columns, Dictionary<string, object> wherePart, bool allFields, int limits)
         {
+            DataTable dt = new DataTable();
             List<Pools> pools = new List<Pools>();
 
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.Gateways.TableName), columns, wherePart, limits, allFields);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Pools pool = new Pools();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.Pools.PoolID))
+                        pool.PoolID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.Pools.PoolName))
+                        pool.PoolName = (string)row[column.ColumnName];
+                }
+                pools.Add(pool);
+            }
             return pools;
         }
 
@@ -29,7 +49,7 @@ namespace Lync_Billing.DB
 
         public void DELETE()
         {
-
+           
         }
     }
 }
