397f23724ba12f46d83e84522bf5a1e9eff88d91
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-25 12:20:29 +0000

    Add GetRatesTableName Function and Functions Parameters
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@49 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/rates.cs
+++ Lync-Billing/DB/rates.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
 
 namespace Lync_Billing.DB
 {
@@ -13,20 +14,28 @@ namespace Lync_Billing.DB
         public DateTime StartingDate { get; set; }
         public DateTime EndingDate { get; set; }
 
-        private int GetRates()
+        public string GetRatesTableName(string gatewayName) 
+        {
+            StringBuilder RatesTableName = new StringBuilder();
+            RatesTableName.Append("Rates_" + gatewayName + "_" + DateTime.Now.Date.ToString("yyyymmdd"));
+           
+            return RatesTableName.ToString();
+        }
+
+        private int GetRates(string ratesTableName, List<string> columns, Dictionary<string, object> wherePart, bool allFields, int limits)
         {
 
             return 0;
         }
 
-        private List<Users> InsertRates()
+        private List<Users> InsertRates(string ratesTableName, List<Rates> rates)
         {
             List<Users> users = new List<Users>();
 
             return users;
         }
 
-        private bool UpdateRates()
+        private bool UpdateRates(string ratesTableName, List<Rates> rates)
         {
             bool status = false;
 
