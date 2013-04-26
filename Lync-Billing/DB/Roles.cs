4aaae425b0be499fb35ad1053c2856ee0a18b2fd
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-26 10:41:03 +0000

    Roles Class
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@70 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/Roles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.Libs;
+
+
+namespace Lync_Billing.DB
+{
+    public class Roles
+    {
+        public DBLib DBRoutines = new DBLib();
+
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+    }
+}
