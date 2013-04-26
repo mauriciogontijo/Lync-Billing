131808ff3e852125996ed2b7ef980cc0dc0c20db
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-26 06:44:39 +0000

    Add Functions prototypes
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@64 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/Sites.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.Libs;
+
+namespace Lync_Billing.DB
+{
+    public class Sites
+    {
+        public DBLib DBRoutines = new DBLib();
+        
+        public int SiteID { get; set; }
+        public string SiteUPN { get; set; }
+        public string CountryCode { get; set; }
+        public string SiteLocation { get; set; }
+
+        public  List<Sites> GetSites(List<string> columns, Dictionary<string, object> wherePart, bool allFields, int limits)
+        {
+            List<Sites> sites = new List<Sites>();
+
+            return sites;
+        }
+
+        public int InsertSite(List<Rates> rates)
+        {
+
+            return 0;
+        }
+
+        public bool UpdateRates(List<Rates> rates)
+        {
+            bool status = false;
+
+
+            return status;
+        }
+
+        public bool DeleteSites(List<Rates> rates) 
+        {
+            bool status = false;
+
+
+            return status;
+        }
+
+        
+
+    }
+}
