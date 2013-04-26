2796d02f6a7039a4e329bd5730190c2df2ab60d1
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-26 11:18:49 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@81 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Roles.cs
+++ Lync-Billing/DB/Roles.cs
@@ -14,5 +14,37 @@ namespace Lync_Billing.DB
         public int RoleID { get; set; }
         public string RoleName { get; set; }
         public string RoleDescription { get; set; }
+
+        public List<Roles> GetRoles(List<string> columns, Dictionary<string, object> wherePart, bool allFields, int limits)
+        {
+            List<Roles> roles = new List<Roles>();
+
+            return roles;
+
+        }
+
+        public int InsertRoles(List<Roles> roles)
+        {
+
+
+            return 0;
+        }
+
+        public bool UpdateRoles(List<Roles> roles)
+        {
+            bool status = false;
+
+
+            return status;
+        }
+
+        public bool DeleteRoles(List<Roles> roles)
+        {
+            bool status = false;
+
+
+            return status;
+        }
+
     }
 }
