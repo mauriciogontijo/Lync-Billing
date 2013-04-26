33dbe5b86fe9db14a852890f4c1ce34bef549023
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-26 11:36:06 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@87 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/UsersRoles.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.Libs;
+
+namespace Lync_Billing.DB
+{
+    public class UsersRoles
+    {
+        public DBLib DBRoutines = new DBLib();
+        
+        public int RolePerUserID { set; get; }
+        public int EmployeeID { get; set; }
+        public int RoleID { get; set; }
+        public int SiteID { get; set; }
+        public int PoolID { get; set; }
+        public int GatewayID { get; set; }
+        public string Notes { get; set; }
+
+        public List<UsersRoles> GetUsersRoles(List<string> columns, Dictionary<string, object> wherePart, bool allFields, int limits)
+        {
+            List<UsersRoles> usersRoles = new List<UsersRoles>();
+
+            return usersRoles;
+        }
+
+        public int InsertUsersRoles(List<UsersRoles> rolesPerUsers)
+        {
+
+
+            return 0;
+        }
+
+        public bool UpdateUsersRoles(List<UsersRoles> rolesPerUsers)
+        {
+            bool status = false;
+
+
+            return status;
+        }
+
+        public bool DeleteFromUsersRoles(List<UsersRoles> rolesPerUsers)
+        {
+            bool status = false;
+
+
+            return status;
+        }
+
+        public bool ValidateUsersRoles(int employeeID, string RoleName)
+        {
+
+            return false;
+        }
+
+    }
+}
