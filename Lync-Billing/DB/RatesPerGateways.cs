d815e4f6a7776c34902ba837748594318c51d5a4
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-26 10:57:25 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@76 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/RatesPerGateways.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.Libs;
+
+namespace Lync_Billing.DB
+{
+    public class RatesPerGateways
+    {
+        public DBLib DBRoutines = new DBLib();
+    }
+}
