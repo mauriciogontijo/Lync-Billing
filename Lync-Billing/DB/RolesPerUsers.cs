d9ca5fba5bfb1095f3b36b6c44e3301cec8eec6c
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-26 11:14:49 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@79 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/RolesPerUsers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.Libs;
+
+namespace Lync_Billing.DB
+{
+    public class RolesPerUsers
+    {
+        public DBLib DBRoutines = new DBLib();
+        
+        public int RolePerUserID { set; get; }
+        public int EmployeeID { get; set; }
+        public int RoleID { get; set; }
+        public int SiteID { get; set; }
+        public int PoolID { get; set; }
+        public int GatewayID { get; set; }
+        public string Notes { get; set; }
+    }
+}
