40048b80330c36c345c56fdf04474398c633185a
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-04-26 11:34:34 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@86 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/GatewaysRates.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.Libs;
+
+namespace Lync_Billing.DB
+{
+    public class GatewaysRates
+    {
+        public DBLib DBRoutines = new DBLib();
+
+        public int RatesPerGatewaysID { set; get; }
+        public int GatewayID { set; get; }
+        public string RatesTaleName { set; get; }
+        public DateTime StartingDate { set; get; }
+        public DateTime EndingDate { set; get; }
+        public string ProviderName { set; get; }
+        public string CurrencyCode { set; get; }
+
+        public List<GatewaysRates> GetGatewaysRates(List<string> columns, Dictionary<string, object> wherePart, bool allFields, int limits)
+        {
+            List<GatewaysRates> gatewaysRates = new List<GatewaysRates>();
+
+            return gatewaysRates;
+        }
+
+        public int InsertGatewaysRates(List<GatewaysRates> ratesPerGateways)
+        {
+
+
+            return 0;
+        }
+
+        public bool UpdateGatewaysRates(List<GatewaysRates> ratesPerGateways)
+        {
+            bool status = false;
+
+
+            return status;
+        }
+
+        public bool DeleteFromGatewaysRates(List<GatewaysRates> ratesPerGateways)
+        {
+            bool status = false;
+
+
+            return status;
+        }
+
+    }
+}
