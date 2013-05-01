cdbc5fedd8aeb496e0da2c8f005f47418c6b993a
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-01 08:49:38 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@165 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Pools.cs
+++ Lync-Billing/DB/Pools.cs
@@ -12,25 +12,7 @@ namespace Lync_Billing.DB
         public DBLib DBRoutines = new DBLib();
         public int PoolID { set; get; }
         public string PoolFQDN { set; get; }
-
-
-        public int InsertPool(List<Pools> pools)
-        {
-            int rowID = 0;
-            Dictionary<string, object> columnsValues = new Dictionary<string, object>();
-
-            foreach (Pools pool in pools)
-            {
-                //Set Part
-                if (pool.PoolFQDN != null)
-                    columnsValues.Add(Enums.GetDescription(Enums.Pools.PoolFQDN), pool.PoolFQDN);
-
-                //Execute Insert
-                rowID = DBRoutines.INSERT(Enums.GetDescription(Enums.Pools.TableName), columnsValues);
-            }
-            return rowID;
-        }
-
+       
         public List<Pools> GetPools(List<string> columns, Dictionary<string, object> wherePart, bool allFields, int limits)
         {
             Pools pool;
@@ -56,52 +38,60 @@ namespace Lync_Billing.DB
             return pools;
         }
 
-        public bool UpdatePools(List<Pools> pools)
+        public int InsertPool(Pools pool)
+        {
+            int rowID = 0;
+            Dictionary<string, object> columnsValues = new Dictionary<string, object>();
+
+            //Set Part
+            if (pool.PoolFQDN != null)
+                columnsValues.Add(Enums.GetDescription(Enums.Pools.PoolFQDN), pool.PoolFQDN);
+            //Execute Insert
+            rowID = DBRoutines.INSERT(Enums.GetDescription(Enums.Pools.TableName), columnsValues);
+
+            return rowID;
+        }
+
+        public bool UpdatePool(Pools pool)
         {
             bool status = false;
 
             Dictionary<string, object> setPart = new Dictionary<string, object>();
             Dictionary<string, object> wherePart = new Dictionary<string, object>();
+         
+            //Where Part
+            wherePart.Add(Enums.GetDescription(Enums.Pools.PoolID), pool.PoolID);
 
-            foreach (Pools pool in pools)
+            //Set Part
+            if (pool.PoolFQDN != null)
+                setPart.Add(Enums.GetDescription(Enums.Pools.PoolFQDN), pool.PoolFQDN);
+
+            //Execute Update
+            status = DBRoutines.UPDATE(Enums.GetDescription(Enums.Pools.TableName), setPart, wherePart);
+
+            if (status == false)
             {
-                //Where Part
-                wherePart.Add(Enums.GetDescription(Enums.Pools.PoolID), pool.PoolID);
-
-                //Set Part
-                if (pool.PoolFQDN != null)
-                    setPart.Add(Enums.GetDescription(Enums.Pools.PoolFQDN), pool.PoolFQDN);
-
-                //Execute Update
-                status = DBRoutines.UPDATE(Enums.GetDescription(Enums.Pools.TableName), setPart, wherePart);
-
-                if (status == false)
-                {
-                    //throw error message
-                }
+                //throw error message
             }
             return true;
         }
 
-        public bool DeletePools(List<Pools> pools)
+        public bool DeletePool(Pools pool)
         {
             bool status = false;
             Dictionary<string, object> wherePart = new Dictionary<string, object>();
+           
+            if ((pool.PoolID).ToString() != null)
+                wherePart.Add(Enums.GetDescription(Enums.Pools.PoolID), pool.PoolID);
 
-            foreach (Pools pool in pools)
+            if (pool.PoolFQDN != null)
+                wherePart.Add(Enums.GetDescription(Enums.Pools.PoolFQDN), pool.PoolFQDN);
+
+            status = DBRoutines.DELETE(Enums.GetDescription(Enums.Pools.TableName), wherePart);
+
+            if (status == false)
             {
-                if ((pool.PoolID).ToString() != null)
-                    wherePart.Add(Enums.GetDescription(Enums.Pools.PoolID), pool.PoolID);
-
-                if (pool.PoolFQDN != null)
-                    wherePart.Add(Enums.GetDescription(Enums.Pools.PoolFQDN), pool.PoolFQDN);
-
-                status = DBRoutines.DELETE(Enums.GetDescription(Enums.Pools.TableName), wherePart);
-
-                if (status == false)
-                {
-                    //throw error message
-                }
+                //throw error message
             }
             return status;
         }
