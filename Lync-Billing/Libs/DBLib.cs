bf7c783763950b1f5c7e0292747bbf019e19111c
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-01 12:38:44 +0000

    Add Documentation
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@200 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/Libs/DBLib.cs
+++ Lync-Billing/Libs/DBLib.cs
@@ -20,6 +20,13 @@ namespace Lync_Billing.Libs
             return new OleDbConnection(connectionString);
         }
 
+        /// <summary>
+        /// Construct Generic Select Statemnet 
+        /// </summary>
+        /// <param name="tableName">DB Table Name</param>
+        /// <param name="whereField">Where statemnet Field</param>
+        /// <param name="whereValue">Where statemnet Value</param>
+        /// <returns> DataTable Object</returns>
         public DataTable SELECT(string tableName, string whereField, object whereValue) 
         {
             DataTable dt = new DataTable();
@@ -52,6 +59,19 @@ namespace Lync_Billing.Libs
             return dt;
         }
 
+        /// <summary>
+        /// Construct Generic Select Statemnet 
+        /// </summary>
+        /// <param name="tableName">DB Table Name</param>
+        /// <param name="fields">List of Fields to be fetched from Database</param>
+        /// <param name="whereClause"> A dictionary which holds the fields and its related values to be able to construct Where Statemnet
+        /// 1. Null if there is no condition
+        /// 2. Dictionary holds fields names and values if there is a condition
+        /// </param>
+        /// <param name="limits">Holds how many rows to be fetched from the database table. 
+        /// 1. 0 if all Rows 
+        /// 2. Value for number of rows</param>
+        /// <returns>DataTable Object</returns>
         public DataTable SELECT(string tableName,List <string> fields, Dictionary<string, object> whereClause, int limits)
         {
             DataTable dt = new DataTable();
@@ -127,6 +147,12 @@ namespace Lync_Billing.Libs
             return dt;
         }
 
+        /// <summary>
+        /// Construct Generic INSERT Statement
+        /// </summary>
+        /// <param name="tableName">DB Table Name</param>
+        /// <param name="columnsValues">Dictionary Holds Fields and Values to be inserted</param>
+        /// <returns>Row ID </returns>
         public int INSERT(string tableName, Dictionary<string, object> columnsValues) 
         {
             StringBuilder fields = new StringBuilder(); fields.Append("(");
@@ -170,6 +196,14 @@ namespace Lync_Billing.Libs
             return recordID;
         }
 
+        /// <summary>
+        /// Construct Generic UPDATE Statement
+        /// </summary>
+        /// <param name="tableName">DB Table Name</param>
+        /// <param name="columnsValues">Dictionary Holds Fields and Values to be inserted</param>
+        /// <param name="idFieldName">ID Field name </param>
+        /// <param name="ID">ID Value</param>
+        /// <returns>Row ID</returns>
         public bool UPDATE(string tableName, Dictionary<string, object> columnsValues,string idFieldName, int ID) 
         {
             StringBuilder fieldsValues = new StringBuilder();
@@ -207,6 +241,13 @@ namespace Lync_Billing.Libs
            
         }
         
+        /// <summary>
+        /// Construct Generic UPDATE Statement
+        /// </summary>
+        /// <param name="tableName">DB Table Name</param>
+        /// <param name="columnsValues">Dictionary Holds Fields and Values to be inserted</param>
+        /// <param name="wherePart">Dictionary Holds Fields and Values to be able to construct Where Statemnet</param>
+        /// <returns></returns>
         public bool UPDATE(string tableName, Dictionary<string, object> columnsValues, Dictionary<string,object> wherePart)
         {
 
@@ -259,9 +300,16 @@ namespace Lync_Billing.Libs
 
         }
 
+        /// <summary>
+        /// Construct Generic DELETE Statement
+        /// </summary>
+        /// <param name="tableName">DB Table Name</param>
+        /// <param name="idFieldName">ID Field Name</param>
+        /// <param name="ID">ID Field Value</param>
+        /// <returns>True if Row has been deleted. </returns>
         public bool DELETE(string tableName, string idFieldName, int ID) 
         {
-             string deleteQuery = string.Format("DELETE FROM ['{1}'] WHERE '{2}={3}'", tableName, idFieldName,ID);
+            string deleteQuery = string.Format("DELETE FROM ['{1}'] WHERE '{2}={3}'", tableName, idFieldName,ID);
 
             OleDbConnection conn = DBInitializeConnection(ConnectionString_Lync);
             OleDbCommand comm = new OleDbCommand(deleteQuery, conn);
@@ -275,11 +323,17 @@ namespace Lync_Billing.Libs
             catch (Exception ex) 
             {
                 System.ArgumentException argEx = new System.ArgumentException("Exception", "ex", ex);
-                throw argEx; 
+                throw argEx;
             }
             finally { conn.Close(); }
         }
         
+        /// <summary>
+        /// Construct Generic DELETE Statement
+        /// </summary>
+        /// <param name="tableName">DB Table Name</param>
+        /// <param name="wherePart">Dictionary Holds Fields and Values to be able to construct Where Statemnet</param>
+        /// <returns>True if Row has been deleted.</returns>
         public bool DELETE(string tableName, Dictionary<string, object> wherePart) 
         {
             StringBuilder whereStatement = new StringBuilder();
