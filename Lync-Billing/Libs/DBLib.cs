342d4845c5f5b71c55e3c508eb3d2ea604943fb9
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-01 12:42:46 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@203 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/Libs/DBLib.cs
+++ Lync-Billing/Libs/DBLib.cs
@@ -6,6 +6,7 @@ using System.Data.OleDb;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Configuration;
 
 
 namespace Lync_Billing.Libs
@@ -13,7 +14,8 @@ namespace Lync_Billing.Libs
     
     public class DBLib
     {
-        public static string ConnectionString_Lync = @"Provider=SQLOLEDB.1;Data Source=10.1.60.55;Persist Security Info=True;Password='=25_ar;p1100';User ID=sa;Initial Catalog=LyncBilling";
+        //public static string ConnectionString_Lync = @"Provider=SQLOLEDB.1;Data Source=10.1.60.55;Persist Security Info=True;Password='=25_ar;p1100';User ID=sa;Initial Catalog=LyncBilling";
+        public static string ConnectionString_Lync = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString.ToString();
 
         private OleDbConnection DBInitializeConnection(string connectionString) 
         {
