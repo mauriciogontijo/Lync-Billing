7f81541a9f05fb1a176dde5f9f548d53a6e991fe
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-01 14:18:49 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@209 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/Libs/JsonTranslator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions; 
+using Lync_Billing.DB;
+
+namespace Lync_Billing.Libs
+{
+    public class JsonTranslator
+    {
+        public static string JsonSerializer<T>(T t) 
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+            MemoryStream ms = new MemoryStream();
+
+            serializer.WriteObject(ms, t);
+           
+            string jsonString = Encoding.UTF8.GetString(ms.ToArray());
+           
+            ms.Close();
+            return jsonString;
+        }
+
+        public static T JsonDeserialize<T>(string jsonString)
+        {
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+           
+            T obj = (T)ser.ReadObject(ms);
+            return obj;
+        }
+
+
+        private static string ConvertJsonDateToDateString(Match m)
+        {
+            string result = string.Empty;
+            DateTime dt = new DateTime(1970, 1, 1);
+            dt = dt.AddMilliseconds(long.Parse(m.Groups[1].Value));
+            dt = dt.ToLocalTime();
+            result = dt.ToString("yyyy-MM-dd HH:mm:ss");
+            return result;
+        }
+        
+        private static string ConvertDateStringToJsonDate(Match m)
+        {
+            string result = string.Empty;
+            DateTime dt = DateTime.Parse(m.Groups[0].Value);
+            dt = dt.ToUniversalTime();
+            TimeSpan ts = dt - DateTime.Parse("1970-01-01");
+            result = string.Format("\\/Date({0}+0800)\\/", ts.TotalMilliseconds);
+            return result;
+        }
+   
+    }
+
+   
+}
