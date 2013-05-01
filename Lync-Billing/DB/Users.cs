3607afc2380133d0b37621cd87cc0d9e25ac751c
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-01 05:45:57 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@140 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Users.cs
+++ Lync-Billing/DB/Users.cs
@@ -2,42 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Lync_Billing.Libs;
 
 namespace Lync_Billing.DB
 {
-    public class Users
+    public class Users : ADUserInfo 
     {
-        
-        public int EmployeeID { get; set; }
-        public string LoginName { get; set; }
-        public string EmailAddress { get; set; }
-        public string PhoneNumber { get; set; }
-        public string SiteName { get; set; }
-        public string PoolFQDN { get; set; }
-        public string UserUPN { get; set; }
-        public bool IsNormalUser { get; set; }
-
-        public List<Users> GetUsers(List<string> columns, Dictionary<string, object> wherePart, bool allFields, int limits) 
+        public ADUserInfo GetUserInfo(string emailAddress)
         {
-            List<Users> users = new List<Users>();
-
-            return users;
-            
+            AdLib adConnector = new AdLib();
+            return adConnector.getUserAttributes(emailAddress);
         }
 
-        public int InsertUsers(List<Users> users) 
-        {
-            
-
-            return 0;
-        }
-
-        public bool UpdateUsers(List<Users> users)
-        {
-            bool status = false;
-
-
-            return status;
-        }
+        public int getUserRole(string emailAddress) { }
+       
     }
 }
