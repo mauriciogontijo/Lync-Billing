f58983cecb2c4639462c8fc0ca43767e296a1f4b
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-01 08:17:42 +0000

    Implement GetGatewaysRates
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@155 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/GatewaysRates.cs
+++ Lync-Billing/DB/GatewaysRates.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Lync_Billing.Libs;
+using System.Data;
 
 namespace Lync_Billing.DB
 {
@@ -10,9 +11,9 @@ namespace Lync_Billing.DB
     {
         public DBLib DBRoutines = new DBLib();
 
-        public int RatesPerGatewaysID { set; get; }
+        public int GatewaysRatesID { set; get; }
         public int GatewayID { set; get; }
-        public string RatesTaleName { set; get; }
+        public string RatesTableName { set; get; }
         public DateTime StartingDate { set; get; }
         public DateTime EndingDate { set; get; }
         public string ProviderName { set; get; }
@@ -20,9 +21,44 @@ namespace Lync_Billing.DB
 
         public List<GatewaysRates> GetGatewaysRates(List<string> columns, Dictionary<string, object> wherePart, bool allFields, int limits)
         {
-            List<GatewaysRates> gatewaysRates = new List<GatewaysRates>();
+            GatewaysRates gatewayRate;
+            DataTable dt = new DataTable();
+            List<GatewaysRates> gatewayRates = new List<GatewaysRates>();
 
-            return gatewaysRates;
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.UsersRoles.TableName), columns, wherePart, limits, allFields);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                gatewayRate = new GatewaysRates();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.GatewaysRates.GatewaysRatesID))
+                        gatewayRate.GatewaysRatesID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.GatewaysRates.GatewayID))
+                        gatewayRate.GatewayID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.GatewaysRates.RatesTableName))
+                        gatewayRate.RatesTableName = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.GatewaysRates.StartingDate))
+                        gatewayRate.StartingDate = (DateTime)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.GatewaysRates.EndingDate))
+                        gatewayRate.EndingDate = (DateTime)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.GatewaysRates.ProviderName))
+                        gatewayRate.ProviderName = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.GatewaysRates.CurrencyCode))
+                        gatewayRate.CurrencyCode = (string)row[column.ColumnName];
+
+                }
+                gatewayRates.Add(gatewayRate);
+            }
+            return gatewayRates;
         }
 
         public int InsertGatewaysRates(List<GatewaysRates> ratesPerGateways)
