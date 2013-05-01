79fb36c1942b62312f37b04dad98049791a56384
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-01 06:30:36 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@150 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/UsersRoles.cs
+++ Lync-Billing/DB/UsersRoles.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Lync_Billing.Libs;
+using System.Data;
 
 namespace Lync_Billing.DB
 {
@@ -20,9 +21,44 @@ namespace Lync_Billing.DB
 
         public List<UsersRoles> GetUsersRoles(List<string> columns, Dictionary<string, object> wherePart, bool allFields, int limits)
         {
-            List<UsersRoles> usersRoles = new List<UsersRoles>();
+            UsersRoles userRole;
+            DataTable dt = new DataTable();
+            List<UsersRoles> roles = new List<UsersRoles>();
 
-            return usersRoles;
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.UsersRoles.TableName), columns, wherePart, limits, allFields);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                userRole = new UsersRoles();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.RoleID))
+                        userRole.RoleID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.SipAccount))
+                        userRole.SipAccount = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.SiteID))
+                        userRole.SiteID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.PoolID))
+                        userRole.PoolID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.GatewayID))
+                        userRole.GatewayID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.UsersRolesID))
+                        userRole.UsersRolesID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.Notes))
+                        userRole.Notes = (string)row[column.ColumnName];
+                    
+                }
+                roles.Add(userRole);
+            }
+            return roles;
+        }
         }
 
         public int InsertUsersRoles(List<UsersRoles> rolesPerUsers)
