883aded4769c048e25f146c2c795c12004174d7d
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-02 05:56:01 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@211 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/Libs/JsonTranslator.cs
+++ Lync-Billing/Libs/JsonTranslator.cs
@@ -27,10 +27,10 @@ namespace Lync_Billing.Libs
 
         public static T Deserialize<T>(string jsonString)
         {
-            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
             MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
-           
-            T obj = (T)ser.ReadObject(ms);
+
+            T obj = (T)serializer.ReadObject(ms);
             return obj;
         }
 
@@ -38,19 +38,19 @@ namespace Lync_Billing.Libs
         private static string JsonDateToDateString(Match m)
         {
             string result = string.Empty;
-            DateTime dt = new DateTime(1970, 1, 1);
-            dt = dt.AddMilliseconds(long.Parse(m.Groups[1].Value));
-            dt = dt.ToLocalTime();
-            result = dt.ToString("yyyy-MM-dd HH:mm:ss");
+            DateTime date= new DateTime(1970, 1, 1);
+            date = date.AddMilliseconds(long.Parse(m.Groups[1].Value));
+            date = date.ToLocalTime();
+            result = date.ToString("yyyy-MM-dd HH:mm:ss");
             return result;
         }
         
         private static string DateStringToJsonDate(Match m)
         {
             string result = string.Empty;
-            DateTime dt = DateTime.Parse(m.Groups[0].Value);
-            dt = dt.ToUniversalTime();
-            TimeSpan ts = dt - DateTime.Parse("1970-01-01");
+            DateTime date = DateTime.Parse(m.Groups[0].Value);
+            date = date.ToUniversalTime();
+            TimeSpan ts = date - DateTime.Parse("1970-01-01");
             result = string.Format("\\/Date({0}+0800)\\/", ts.TotalMilliseconds);
             return result;
         }
