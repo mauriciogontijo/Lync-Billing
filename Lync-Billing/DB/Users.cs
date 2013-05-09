01316c3216bafa9f9564fec1cd38d0215c5c9c1b
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-09 06:29:32 +0000

    Users Class
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@244 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/Users.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.Libs;
+using System.Data;
+
+namespace Lync_Billing.DB
+{
+    public class Users
+    {
+        public static DBLib DBRoutines = new DBLib();
+
+        public string SipAccount { get; set; }
+        public string SiteName { get; set; }
+
+        /// <summary>
+        /// Get All Related User Information From Active Directory
+        /// </summary>
+        /// <param name="emailAddress">User Email Address</param>
+        /// <returns>Onject Holds all related user information</returns>
+        public static ADUserInfo GetUserInfo(string emailAddress)
+        {
+            AdLib adConnector = new AdLib();
+            return adConnector.getUserAttributes(emailAddress);
+        }
+
+        /// <summary>
+        /// Get User Role
+        /// </summary>
+        /// <param name="sipAccount">User Sip Account</param>
+        /// <returns>List of all User Roles</returns>
+        public static List<UserRole> GetUserRoles(string sipAccount)
+        {
+            UserRole userRole;
+            DataTable dt = new DataTable();
+            List<UserRole> roles = new List<UserRole>();
+
+            Dictionary<string, object> wherePart = new Dictionary<string, object>();
+            wherePart.Add("SipAccount", sipAccount);
+
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.UsersRoles.TableName), null, wherePart, 0);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                userRole = new UserRole();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.RoleID) && row[column.ColumnName] != System.DBNull.Value)
+                        userRole.RoleID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.SipAccount) && row[column.ColumnName] != System.DBNull.Value)
+                        userRole.SipAccount = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.SiteID) && row[column.ColumnName] != System.DBNull.Value)
+                        userRole.SiteID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.PoolID) && row[column.ColumnName] != System.DBNull.Value)
+                        userRole.PoolID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.GatewayID) && row[column.ColumnName] != System.DBNull.Value)
+                        userRole.GatewayID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.UsersRolesID) && row[column.ColumnName] != System.DBNull.Value)
+                        userRole.UsersRolesID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.UsersRoles.Notes) && row[column.ColumnName] != System.DBNull.Value)
+                        userRole.Notes = (string)row[column.ColumnName];
+
+                }
+                roles.Add(userRole);
+            }
+            return roles;
+        }
+       
+    }
+}
