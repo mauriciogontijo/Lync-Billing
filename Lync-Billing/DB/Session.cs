fa92c2a4a4b31c186dbaa939ce014c584aa89c44
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-09 11:42:38 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@265 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/Session.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.Libs;
+
+namespace Lync_Billing.DB
+{
+    public class Session
+    {
+        public int EmployeeID { get; set; }
+        public string DisplayName { get; set; }
+        public string EmailAddress { get; set; }
+        public string SipAccount { get; set; }
+        public string SiteName { get; set; }
+        public string ActiveRoleName { get; set; }
+        public string SrcIPAddress { get; set; }
+        
+        public DateTime LastLoginTime { get; set; }
+        public int Duration { get; set; }
+
+        public List<UserRole> userRoles { get; set; }
+    }
+
+    
+}
