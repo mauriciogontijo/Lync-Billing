5e83d2b342ef0ef12485b9d1a5557c5250334124
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-10 07:31:48 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@280 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/Session.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.Libs;
+
+namespace Lync_Billing.DB
+{
+    public class Session
+    {
+        public static List<Session> sessions = new List<Session>();
+       
+        public int EmployeeID { get; set; }
+        public string DisplayName { get; set; }
+        public string EmailAddress { get; set; }
+        public string SipAccount { get; set; }
+        public string SiteName { get; set; }
+        public string ActiveRoleName { get; set; }
+        public string SrcIPAddress { get; set; }
+        
+        public DateTime LastLoginTime { get; set; }
+        public int Duration { get; set; }
+
+        public List<UserRole> userRoles { get; set; }
+    
+        public static void AddUserSession(Session userSession)
+        {
+            if (!sessions.Contains(userSession))
+                sessions.Add(userSession);
+        }
+
+        public static void RemoveUserSession(string emailAddress) 
+        {
+            foreach(Session session in sessions)
+            {
+                if (session.EmailAddress == emailAddress)
+                {
+                    sessions.Remove(session);
+                    break;
+                }
+            }
+        }
+    }
+
+    
+}
---
+++ Lync-Billing/DB/UserSession.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lync_Billing.DB
+{
+    public class UserSession
+    { 
+    }
+}
