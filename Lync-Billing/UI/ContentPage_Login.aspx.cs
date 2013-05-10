404810cbc8a9cd792d44d5d8f394199d25cdd644
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-10 11:30:54 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@312 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/ContentPage_Login.aspx.cs
+++ Lync-Billing/UI/ContentPage_Login.aspx.cs
@@ -10,9 +10,12 @@ namespace Lync_Billing.UI
 {
     public partial class ContentPage_Login : System.Web.UI.Page
     {
+        static string hostname = string.Empty;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             local_ip_address.Value = HttpContext.Current.Request.Headers.GetValues("Host")[0];
+            hostname = HttpContext.Current.Request.Headers.GetValues("Host")[0];
         }
     }
 }
