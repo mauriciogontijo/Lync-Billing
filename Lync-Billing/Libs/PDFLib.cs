f40913879dfa6577b220a9d3b4199f434729a9d7
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-11 08:29:18 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@330 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/Libs/PDFLib.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+
+namespace Lync_Billing.Libs
+{
+    public class PDFLib 
+    {
+        StyleSheet styles = new StyleSheet();
+        string fontPath = Environment.GetEnvironmentVariable("SystemRoot") + "\\fonts\\verdana.ttf";
+        //FontFactory fontFactory = b
+
+        public string WriteToPDF(string htmlString) 
+        {
+            return null;
+        }
+
+    }
+}
