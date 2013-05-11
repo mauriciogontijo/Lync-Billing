e2fe2ad2e02dfd366308b0c0bb4146c691d84e3e
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-11 14:14:52 +0000

    Holds Datastores and It related Models
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@333 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/UI/DBStores.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ext.Net;
+using Lync_Billing.DB;
+
+namespace Lync_Billing.UI
+{
+    public class DBStores
+    {
+        public static Model PhoneCallModel(List<string> columns)
+        {
+            Model model;
+
+            if (columns != null)
+            {
+                model = new Model()
+                {
+                    ID = "phoneCallModel",
+                    IDProperty = "SessionIdTime",
+                };
+
+                foreach (string column in columns)
+                {
+                    model.Fields.Add(new ModelField(column));
+                }
+            }
+            else
+            {
+                model = new Model()
+                {
+                    ID = "phoneCallModel",
+                    IDProperty = "SessionIdTime",
+                    Fields = 
+                    {
+                        new ModelField("SessionIdTime"),
+                        new ModelField("SessionIdSeq"),
+                        new ModelField("ResponseTime"),
+                        new ModelField("SessionEndTime"),
+                        new ModelField("SourceUserUri"),
+                        new ModelField("SourceNumberUri"),
+                        new ModelField("DestinationNumberUri"),
+                        new ModelField("ServerFQDN"),
+                        new ModelField("PoolFQDN"),
+                        new ModelField("Marker_CallToCountry"),
+                        new ModelField("marker_CallType"),
+                        new ModelField("Duration"),
+                        new ModelField("Marker_CallCost"),
+                        new ModelField("UI_UpdatedByUser"),
+                        new ModelField("UI_MarkedOn"),
+                        new ModelField("UI_IsPersonal"),
+                        new ModelField("UI_Dispute"),
+                        new ModelField("UI_IsInvoiced")
+                    }
+                };
+            }
+            return model;
+        }
+
+        public static Store PhoneCallsStore(List<string> columns, Dictionary<string, object> wherePart, int limits)
+        {
+            Store store = new Store()
+            {
+                ID = "PhonceCallsStore",
+                DataSource = PhoneCall.GetPhoneCalls(columns, wherePart, limits),
+                Model = { PhoneCallModel(columns) }
+            };
+            return store;
+        }
+
+      
+    }
+}
