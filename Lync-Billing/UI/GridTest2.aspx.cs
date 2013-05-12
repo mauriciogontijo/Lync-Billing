d498b1f4904d3dfe93ad94cce1390b84f1409054
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-12 10:28:08 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@343 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/GridTest2.aspx.cs
+++ Lync-Billing/UI/GridTest2.aspx.cs
@@ -11,8 +11,10 @@ namespace Lync_Billing.UI
 {
     public partial class GridTest2 : System.Web.UI.Page
     {
+        GridPanel grid;
         protected void Page_Load(object sender, EventArgs e)
         {
+            
             string sipAccount = "SGhaida@ccc.gr";
             Dictionary<string,object> wherePart = new Dictionary<string,object>();
             List<string> columns = new List<string>();
@@ -29,7 +31,26 @@ namespace Lync_Billing.UI
             Ext.Net.Panel panel = new Ext.Net.Panel();
             panel.ID = "GridPanel";
 
-            GridPanel grid = Grids.PhoneCallsGrid(columns, wherePart, 10);
+            grid = Grids.PhoneCallsGrid(columns, wherePart, 10);
+            grid.SelectionModel.Add(new RowSelectionModel()
+            {
+                AllowDeselect = true,
+                Mode = SelectionMode.Single
+            });
+          
+            //Grid editor plugin : enables editing
+            grid.Plugins.Add(new CellEditing() 
+            {
+                ClicksToEdit = 2
+            });
+            Ext.Net.TextField textField = new TextField();
+            textField.ID = "SessionTime";
+            //Add Editor to SessionTime 
+            grid.ColumnModel.Columns[2].Editor.Add(textField);
+            //Formating Date
+            grid.ColumnModel.Columns[0].Renderer.Handler = "return Ext.util.Format.date(value, 'Y-m-d');";
+            grid.ColumnModel.Columns[1].Renderer.Handler = "return Ext.util.Format.date(value, 'Y-m-d');";                        
+                                    
             Viewport viewport = new Viewport();
             viewport.ID = "viewport";
             viewport.Layout ="Border";
@@ -54,19 +75,33 @@ namespace Lync_Billing.UI
             markingStatus.QueryMode = DataLoadMode.Local;
             markingStatus.TypeAhead = true;
             markingStatus.TriggerAction = TriggerAction.All;
-            
+
             Toolbar toolbar = new Toolbar();
             toolbar.ID = "Toolbar";
 
+            Ext.Net.Button save = new Ext.Net.Button();
+            save.Selectable = true;
+            save.ID = "Save";
+            save.DirectClick +=save_DirectClick;
+
             toolbar.Add(markingStatus);
             grid.TopBar.Add(toolbar);
-
+            grid.Buttons.Add(save);
             panel.Add(grid);
             viewport.Add(panel);
-
+           
 
             form1.Controls.Add(viewport);
             
         }
+
+        private void save_DirectClick(object sender, DirectEventArgs e)
+        {
+            (grid.GetStore()).Sync();
+        }
+         public string formatDateTime(string value) {
+             return "2013-10-10";
+            }
+      
      }
 }
