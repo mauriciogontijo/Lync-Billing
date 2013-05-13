4f7edc61b25f65c747d7f8843723cd9b6c1efb81
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-13 11:17:06 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@358 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/User_Inner.aspx.cs
+++ Lync-Billing/UI/User_Inner.aspx.cs
@@ -4,14 +4,43 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Lync_Billing.DB;
+using Ext.Net;
 
 namespace Lync_Billing.UI
 {
     public partial class User_Inner : System.Web.UI.Page
     {
+        GridPanel UserPhoneCallsHistoryGrid;
         protected void Page_Load(object sender, EventArgs e)
         {
+            string SipAccount = ((UserSession)Session.Contents["UserData"]).SipAccount;
+            Dictionary<string, object> wherePart = new Dictionary<string, object>();
+            List<string> columns = new List<string>();
 
+            wherePart.Add("SourceUserUri", SipAccount);
+            columns.Add("SessionIdTime");
+            columns.Add("DestinationNumberUri");
+            columns.Add("Duration");
+            columns.Add("marker_CallCost");
+
+            UserPhoneCallsHistoryGrid = Grids.PhoneCallsGrid(columns, wherePart, 7);
+            Ext.Net.TextField textField = new TextField();
+            textField.ID = "SessionTime";
+            UserPhoneCallsHistoryGrid.ColumnModel.Columns[0].Renderer.Handler = "return Ext.util.Format.date(value, 'Y-m-d');";
+
+            UserPhoneCallsHistoryGrid.ID = "PhoneCallsGrid";
+            UserPhoneCallsHistoryGrid.Layout = "Table";
+            UserPhoneCallsHistoryGrid.Width = 465;
+            UserPhoneCallsHistoryGrid.ColumnModel.Columns[0].Text = "Date";
+            UserPhoneCallsHistoryGrid.ColumnModel.Columns[1].Text = "Destination";
+            UserPhoneCallsHistoryGrid.ColumnModel.Columns[1].Width = 165;
+
+            UserPhoneCallsHistoryGrid.ColumnModel.Columns[3].Text = "Cost";
+            UserPhoneCallsHistoryGrid.Header = true;
+            UserPhoneCallsHistoryGrid.Title = "Calls History";
+
+            UserPhoneCallsHistoryPH.Controls.Add(UserPhoneCallsHistoryGrid);
         }
     }
 }
