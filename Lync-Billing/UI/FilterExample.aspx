0847f3cb1892f487e54f581a9799422ed95b1348
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-14 07:23:21 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@370 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/FilterExample.aspx
+++ Lync-Billing/UI/FilterExample.aspx
@@ -1,4 +1,5 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FilterExample.aspx.cs" Inherits="Lync_Billing.UI.FilterExample" %>
+<%@ Register assembly="Ext.Net" namespace="Ext.Net" tagprefix="ext" %>
 
 <!DOCTYPE html>
 
@@ -9,6 +10,7 @@
 <body>
     <form id="form1" runat="server">
     <div>
+        <ext:ResourceManager id="resourceManager" runat="server" Theme="Gray" />
         <asp:PlaceHolder ID="ph" runat="server"></asp:PlaceHolder>
     </div>
     </form>
--- Lync-Billing/UI/FilterExample.aspx.designer.cs
+++ Lync-Billing/UI/FilterExample.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace Lync_Billing.UI {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// resourceManager control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.ResourceManager resourceManager;
+        
+        /// <summary>
         /// ph control.
         /// </summary>
         /// <remarks>
