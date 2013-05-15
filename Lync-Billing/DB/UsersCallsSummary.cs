74a384b92701f231dc2d37c4fde591698bced29f
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-15 12:25:38 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@423 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/UsersCallsSummary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.Libs;
+namespace Lync_Billing.DB
+{
+    public class UsersCallsSummary
+    {
+
+        private static 
+
+        int BusinessCallsCount { get; set; }
+        int PersonalCallsCount { get; set; }
+        int BusinessCallsDuration { get; set; }
+        int PersonalCallsDuration { get; set; }
+        int BusinessCallsCost { get; set; }
+        int PersonalCallsCost { get; set; }
+        int UnMarkedCalls { get; set; }
+        int NumberOfDisputedCalls { get; set; }
+
+    }
+}
