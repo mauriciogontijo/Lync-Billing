156fe59b5444de72a226101e31b537c6a1beb3a1
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-15 14:33:27 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@432 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/UsersCallsSummary.cs
+++ Lync-Billing/DB/UsersCallsSummary.cs
@@ -24,12 +24,13 @@ namespace Lync_Billing.DB
 
         int NumberOfDisputedCalls { get; set; }
 
-        public static UsersCallsSummary(string sipAccount, DateTime startingDate, DateTime endingDate)
+        public static UsersCallsSummary GetUsersCallsSummary(string sipAccount, DateTime startingDate, DateTime endingDate)
         {
             wherePart = new Dictionary<string, object>();
             columns = new List<string>();
 
             DataTable dt = new DataTable();
+            UsersCallsSummary userSummary = new UsersCallsSummary();
 
             wherePart.Add("SourceUserUri", sipAccount);
             wherePart.Add("startingDate", startingDate);
@@ -37,7 +38,64 @@ namespace Lync_Billing.DB
 
             dt = DBRoutines.SELECT_USER_STATISTICS(Enums.GetDescription(Enums.PhoneCalls.TableName), wherePart);
 
+            
+            foreach (DataRow row in dt.Rows)
+            {
+                if(row[dt.Columns["PhoneCallType"]].ToString() == "NO")
+                {
+                    if (row[dt.Columns["ui_IsPersonal"]] != System.DBNull.Value)
+                        userSummary.BusinessCallsCount = (int)row[dt.Columns["ui_IsPersonal"]];
+                    else
+                        userSummary.BusinessCallsCount = 0;
 
+                    if (row[dt.Columns["TotalCost"]] != System.DBNull.Value)
+                        userSummary.BusinessCallsCost = (int)row[dt.Columns["TotalCost"]];
+                    else
+                        userSummary.BusinessCallsCost = 0;
+
+                    if (row[dt.Columns["TotalDuration"]] != System.DBNull.Value)
+                        userSummary.BusinessCallsDuration = (int)row[dt.Columns["TotalDuration"]];
+                    else
+                        userSummary.BusinessCallsDuration = 0;
+                }
+
+                else if (row[dt.Columns["PhoneCallType"]].ToString() == "YES")
+                {
+                    if (row[dt.Columns["ui_IsPersonal"]] != System.DBNull.Value)
+                        userSummary.PersonalCallsCount = (int)row[dt.Columns["ui_IsPersonal"]];
+                    else
+                        userSummary.PersonalCallsCount = 0;
+
+                    if (row[dt.Columns["TotalCost"]] != System.DBNull.Value)
+                        userSummary.PersonalCallsCost = (int)row[dt.Columns["TotalCost"]];
+                    else
+                        userSummary.PersonalCallsCost = 0;
+
+                    if (row[dt.Columns["TotalDuration"]] != System.DBNull.Value)
+                        userSummary.PersonalCallsDuration = (int)row[dt.Columns["TotalDuration"]];
+                    else
+                        userSummary.PersonalCallsDuration = 0;
+                }
+
+                else if (row[dt.Columns["PhoneCallType"]] == System.DBNull.Value)
+                {
+                    if (row[dt.Columns["ui_IsPersonal"]] != System.DBNull.Value)
+                        userSummary.UnmarkedCallsCount = (int)row[dt.Columns["ui_IsPersonal"]];
+                    else
+                        userSummary.UnmarkedCallsCount = 0;
+
+                    if (row[dt.Columns["TotalCost"]] != System.DBNull.Value)
+                        userSummary.UnmarkedCallsCost = (int)row[dt.Columns["TotalCost"]];
+                    else
+                        userSummary.UnmarkedCallsCost = 0;
+
+                    if (row[dt.Columns["TotalDuration"]] != System.DBNull.Value)
+                        userSummary.UnmarkedCallsDuartion = (int)row[dt.Columns["TotalDuration"]];
+                    else
+                        userSummary.UnmarkedCallsDuartion = 0;
+                }
+            }
+            return userSummary;
         }
 
     }
