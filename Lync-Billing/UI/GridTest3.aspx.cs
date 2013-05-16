5e32a77b3aa7a8fbbf26c4ea68a5fd589c55a6ec
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-16 05:42:40 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@435 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/GridTest3.aspx.cs
+++ Lync-Billing/UI/GridTest3.aspx.cs
@@ -12,61 +12,14 @@ namespace Lync_Billing.UI
     public partial class GridTest3 : System.Web.UI.Page
     {
         Dictionary<string, object> wherePart = new Dictionary<string, object>();
-        List<string> columns = new List<string>();
+        UsersCallsSummary userSummary;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            string SipAccount = ((UserSession)Session.Contents["UserData"]).SipAccount;
+            userSummary = new UsersCallsSummary();
+            string SipAccount = "SGhaida@ccc.gr"; //((UserSession)Session.Contents["UserData"]).SipAccount;
 
-
-            wherePart.Add("SourceUserUri", SipAccount);
-
-            columns.Add("SessionIdTime");
-            columns.Add("marker_CallToCountry");
-            columns.Add("DestinationNumberUri");
-            columns.Add("Duration");
-            columns.Add("marker_CallCost");
-            columns.Add("ui_IsPersonal");
-            columns.Add("ui_MarkedOn");
-            columns.Add("ui_IsInvoiced");
-
-            PhoneCallStore.DataSource = PhoneCall.GetPhoneCalls(columns, wherePart, 0);
-            PhoneCallStore.DataBind();
-        }
-
-        protected void PhoneCallStore_AfterRecordUpdated(object sender, AfterRecordUpdatedEventArgs e)
-        {
-
-        }
-
-        protected void PhoneCallStore_AfterStoreChanged(object sender, AfterStoreChangedEventArgs e)
-        {
-
-        }
-
-        protected void PhoneCallStore_AfterDirectEvent(object sender, AfterDirectEventArgs e)
-        {
-
-        }
-
-        protected void PhoneCallStore_BeforeDirectEvent(object sender, BeforeDirectEventArgs e)
-        {
-
-        }
-
-        protected void PhoneCallStore_BeforeRecordUpdated(object sender, BeforeRecordUpdatedEventArgs e)
-        {
-
-        }
-
-        protected void PhoneCallStore_BeforeStoreChanged(object sender, BeforeStoreChangedEventArgs e)
-        {
-
-        }
-
-        protected void GridSubmitChanges_Click(object sender, DirectEventArgs e)
-        {
-
+            this.UserCallSummary.Text = UsersCallsSummary.GetUsersCallsSummary(SipAccount, DateTime.Now.AddYears(-1), DateTime.Now).ToString();
         }
     }
 }
