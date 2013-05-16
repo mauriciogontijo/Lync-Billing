e829df91a92a0b9088248a4d77acf4a55d807104
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-16 10:14:47 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@448 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/User_Home.aspx.cs
+++ Lync-Billing/UI/User_Home.aspx.cs
@@ -59,37 +59,9 @@ namespace Lync_Billing.UI
             ComponentLoader.Config ComponentLoaderConfig = new ComponentLoader.Config();
             ComponentLoaderConfig = ComponentLoader.ToConfig();
             ComponentLoader UserPhoneCallsSummaryLoader = new ComponentLoader(ComponentLoaderConfig);
-            this.UserPhoneCallsSummary.LoadContent(ComponentLoader.ToConfig(new List<AbstractComponent>() {
-                new Ext.Net.Panel {
-                    Title="Personal Calls Overview",
-                    Icon = Icon.Phone,
-                    Html = String.Format(
-                        "<div class='block-body wauto m5'><p>" + 
-                        "<p>You have a total of {0} {1} phone calls, they add up to almost {2} minutes.</p>" +
-                        "<p>The net calculated cost is {3} euros.</p></div>",
-                        userSummary.PersonalCallsCount, "personal", userSummary.PersonalCallsDuration/=60, userSummary.PersonalCallsCost)
-                },
-
-                new Ext.Net.Panel {
-                    Title="Business Calls Overview",
-                    Icon = Icon.Phone,
-                    Html = String.Format(
-                        "<div class='block-body wauto m5'>" + 
-                        "<p>You have a total of {0} {1} phone calls, they add up to almost {2} minutes.</p>" + 
-                        "<p>The net calculated cost is {3} euros.</p></div>",
-                        userSummary.BusinessCallsCount, "business", userSummary.BusinessCallsDuration/=60, userSummary.BusinessCallsCost)
-                },
-
-                new Ext.Net.Panel {
-                    Title="Unmarked Calls Overview",
-                    Icon = Icon.Phone,
-                    Html = String.Format(
-                        "<div class='block-body wauto m5'>" + 
-                        "<p>You have a total of {0} {1} phone calls, they add up to almost {2} minutes.</p>" + 
-                        "<p>The net calculated cost is {3} euros.</p></div>",
-                        userSummary.UnmarkedCallsCount, "unmarked", userSummary.UnmarkedCallsDuartion/=60, userSummary.UnmarkedCallsCost)
-                }
-            }));*/
+            this.UserPhoneCallsSummary.LoadContent(ComponentLoader.ToConfig(
+                //......
+            );*/
         }
 
 
@@ -100,10 +72,8 @@ namespace Lync_Billing.UI
 
             UsersCallsSummary userSummary = new UsersCallsSummary();
             userSummary = UsersCallsSummary.GetUsersCallsSummary(SipAccount, DateTime.Now.AddYears(-1), DateTime.Now);
-
-            JavaScriptSerializer serializer = new JavaScriptSerializer();
-
-            return serializer.Serialize(ComponentLoader.ToConfig(new List<AbstractComponent>() {
+            
+            return ComponentLoader.ToConfig(new List<AbstractComponent>() {
                 new Ext.Net.Panel { 
                     Title="Personal Calls Overview",
                     Icon = Icon.Phone,
@@ -133,7 +103,7 @@ namespace Lync_Billing.UI
                         "<p>The net calculated cost is {3} euros.</p></div>",
                         userSummary.UnmarkedCallsCount, "unmarked", userSummary.UnmarkedCallsDuartion/=60, userSummary.UnmarkedCallsCost)
                 }
-            }));
+            });
         }
     }
 }
