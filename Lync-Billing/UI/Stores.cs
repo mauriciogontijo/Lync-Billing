9548b99c60dab66256e3a0819f65c648ccb9d0ac
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-19 11:59:38 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@589 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/Stores.cs
+++ Lync-Billing/UI/Stores.cs
@@ -8,43 +8,8 @@ namespace Lync_Billing.UI
 {
     public class Stores
     {
-        public  List<PhoneCall> phoneCallsHistoryStoreDataSource = new List<PhoneCall>();
-        public  List<PhoneCall> phoeCallsmanagementStoreDataSource = new List<PhoneCall>();
-        public  List<UsersCallsSummaryChartData> phoneCallsSummaryChartData = new List<UsersCallsSummaryChartData>();
-
-        public void SET_PhoneCallsHistoryStoreDataSource(List<PhoneCall> phoneCallsHistoryStoreDataSource) 
-        {
-            this.phoneCallsHistoryStoreDataSource = phoneCallsHistoryStoreDataSource;
-        }
-
-        public void SET_PhoneCallsHistoryStoreDataSource(List<PhoneCall> phoeCallsmanagementStoreDataSource)
-        {
-            this.phoeCallsmanagementStoreDataSource = phoeCallsmanagementStoreDataSource;
-        }
-
-        public void SET_PhoneCallsHistoryStoreDataSource(List<UsersCallsSummaryChartData> phoneCallsSummaryChartData)
-        {
-            this.phoneCallsSummaryChartData = phoneCallsSummaryChartData;
-        }
-
-        public List<PhoneCall> GET_PhoneCallsHistoryStoreDataSource()
-        {
-            return phoneCallsHistoryStoreDataSource;
-        }
-
-        public List<PhoneCall> GET_PhoneCallsHistoryStoreDataSource()
-        {
-            return phoeCallsmanagementStoreDataSource;
-        }
-
-        public List<UsersCallsSummaryChartData> GET_PhoneCallsHistoryStoreDataSource()
-        {
-            return  phoneCallsSummaryChartData;
-        }
-
-        public static Stores Stores() 
-        {
-            return new Stores();
-        }
+        public static List<PhoneCall> phoneCallsHistoryStoreDataSource;
+        public static List<PhoneCall> phoeCallsmanagementStoreDataSource;
+        public static List<UsersCallsSummaryChartData> phoneCallsSummaryChartData;
     }
 }
