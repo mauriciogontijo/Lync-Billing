c555d5aa1adde83b0d0085d458ef8c3ba288a20b
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-19 13:27:19 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@594 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/Stores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.DB;
+
+namespace Lync_Billing.UI
+{
+    public class Stores
+    {
+        public List<PhoneCall> phoneCallsHistoryStoreDataSource;
+        public List<PhoneCall> phoeCallsmanagementStoreDataSource;
+        public List<UsersCallsSummaryChartData> phoneCallsSummaryChartData;
+    }
+}
