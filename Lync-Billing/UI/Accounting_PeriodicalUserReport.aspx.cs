9a32b8780015be3cf06297eb324691d0dc0e8b80
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-23 06:08:56 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@700 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/Accounting_PeriodicalUserReport.aspx.cs
+++ Lync-Billing/UI/Accounting_PeriodicalUserReport.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Lync_Billing.DB;
+using System.Xml;
+using System.Xml.Xsl;
 
 namespace Lync_Billing.UI
 {
@@ -11,7 +14,30 @@ namespace Lync_Billing.UI
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //If the user is not loggedin, redirect to Login page.
+            if (Session.Contents["UserData"] == null)
+            {
+                Response.Redirect("~/UI/Login.aspx");
+            }
+            else
+            {
+                bool status = new Boolean();
+                status = false;
 
+                UserSession session = new UserSession();
+                session = (UserSession)Session.Contents["UserData"];
+
+                foreach (UserRole role in session.Roles)
+                {
+                    if (role.RoleID == 7 || role.RoleID == 1)
+                        status = true;
+                }
+
+                if (status == false)
+                {
+                    Response.Redirect("~/UI/Login.aspx");
+                }
+            }
         }
     }
 }
