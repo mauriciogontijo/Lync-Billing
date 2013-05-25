01d662acdb1452f64353250d82c5ee52462e8bda
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-25 07:47:42 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@735 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/Accounting_MonthlyUserReport.aspx.cs
+++ Lync-Billing/UI/Accounting_MonthlyUserReport.aspx.cs
@@ -15,6 +15,8 @@ namespace Lync_Billing.UI
 {
     public partial class Accounting_MonthlyUserReport : System.Web.UI.Page
     {
+        Store AccountingStore;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             //If the user is not loggedin, redirect to Login page.
@@ -32,6 +34,57 @@ namespace Lync_Billing.UI
                     Response.Redirect("~/UI/User_Dashboard.aspx");
                 }
             }
+        }
+
+        public string GetSiteName(int siteID) 
+        {
+            Dictionary<string,object> wherePart = new Dictionary<string,object>();
+            wherePart.Add("SiteID", siteID);
+
+            List<Site> sites = DB.Site.GetSites(null, wherePart, 0);
+
+            return sites[0].SiteName;
+        }
+
+        public bool ValidateAccountantSite(string employeeID) 
+        {
+            UserSession session = new UserSession();
+            session = (UserSession)Session.Contents["UserData"];
+
+            List<UserRole> userRoles = session.Roles;
+
+            foreach (UserRole role in userRoles) 
+            {
+                if (GetSiteName(role.SiteID) == GetSipAccountSite(employeeID) && role.RoleID == 7) 
+                    return true;
+            }
+            return false;
+        }
+
+        public string GetSipAccount(string employeeID) 
+        {
+            Dictionary<string, object> whereStatement = new Dictionary<string, object>();
+            List<string> fields = new List<string>();
+            List<Users> users = new List<Users>();
+
+            whereStatement.Add("UserID", employeeID);
+            fields.Add("SipAccount");
+
+            users = Users.GetUsers(fields, whereStatement, 0);
+            return users[0].SipAccount;
+        }
+
+        public string GetSipAccountSite(string employeeID)
+        {
+            Dictionary<string, object> whereStatement = new Dictionary<string, object>();
+            List<string> fields = new List<string>();
+            List<Users> users = new List<Users>();
+
+            whereStatement.Add("UserID", employeeID);
+            fields.Add("SipAccount");
+
+            users = Users.GetUsers(fields, whereStatement, 0);
+            return users[0].SiteName;
         }
     }
 }
