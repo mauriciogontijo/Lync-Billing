94ab203aed4a0e115a9856841d7fbbfb5ecfaa34
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-28 05:31:24 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@756 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Delegates.cs
+++ Lync-Billing/DB/Delegates.cs
@@ -14,18 +14,48 @@ namespace Lync_Billing.DB
 
         private static DBLib DBRoutines = new DBLib();
 
-        public string GetSipAccount(string delegateAccount) 
+        public Delegates GetBySipAccount(string delegateAccount) 
         {
-            Dictionary<string,object> whereStatemnet = new Dictionary<string,object>();
+            Delegates delegates = new Delegates();
+
             DataTable dt = new DataTable();
             dt = DBRoutines.SELECT(Enums.GetDescription(Enums.Delegates.TableName), "DelegateAccount", delegateAccount);
 
-            return null;
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.Delegates.DelegateAccount) && row[column.ColumnName] != System.DBNull.Value)
+                        delegates.DelegateAccount = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.Delegates.SipAccount) && row[column.ColumnName] != System.DBNull.Value)
+                        delegates.SipAccount = (string)row[column.ColumnName];
+                }
+            }
+
+            return delegates;
         }
 
-        public string getDelegateAccount(string sipAccount) 
+        public Delegates GetByDelegateAccount(string sipAccount) 
         {
-            return null;
+            Delegates delegates = new Delegates();
+
+            DataTable dt = new DataTable();
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.Delegates.TableName), "SipAccount", sipAccount);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.Delegates.DelegateAccount) && row[column.ColumnName] != System.DBNull.Value)
+                        delegates.DelegateAccount = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.Delegates.SipAccount) && row[column.ColumnName] != System.DBNull.Value)
+                        delegates.SipAccount = (string)row[column.ColumnName];
+                }
+            }
+
+            return delegates;
         }
 
         public bool UpadeDelegate(string sipAccount) 
