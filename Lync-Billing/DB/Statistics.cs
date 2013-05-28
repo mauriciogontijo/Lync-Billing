daa654269e12b1f0a567db8dde02c3b0a7f4a8c5
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-28 08:41:39 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@779 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Statistics.cs
+++ Lync-Billing/DB/Statistics.cs
@@ -9,6 +9,19 @@ namespace Lync_Billing.DB
     {
     }
 
-    
+
+    public class TopCountries 
+    {
+        public string CountryName { private set; get; }
+        public long TotalDuration { private set; get; }
+        public decimal TotalCost { private set; get; }
+    }
+
+    public class TopDestinations 
+    {
+        public string PhoneNumber { private set; get; }
+        public string Internal { private set; get; }
+        public long NumberOfPhoneCalls { private set; get; }
+    }
 
 }
