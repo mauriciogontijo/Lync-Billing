c441522142035089c3196e9bce51868a4fca881c
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-28 05:15:17 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@752 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Delegates.cs
+++ Lync-Billing/DB/Delegates.cs
@@ -7,5 +7,34 @@ namespace Lync_Billing.DB
 {
     public class Delegates
     {
+        public string SipAccount { get; set; }
+        public string DelegateAccount { get; set; }
+
+        public string GetSipAccount(string delegateAccount) 
+        {
+            return null;
+        }
+
+        public string getDelegateAccount(string sipAccount) 
+        {
+            return null;
+        }
+
+        public bool UpadeDelegate(string sipAccount) 
+        {
+            return false;
+        }
+
+        public bool DeleteDelegate(string sipAccount) 
+        {
+            return false;
+        }
+
+        public bool AddDelegate(string sipAccount) 
+        {
+            return false;
+        }
     }
+
+    
 }
