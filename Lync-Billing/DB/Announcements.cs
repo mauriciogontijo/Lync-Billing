aa5b25372475e3d8849874c16609fbf04375efcb
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-29 12:07:19 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@856 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Announcements.cs
+++ Lync-Billing/DB/Announcements.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using Lync_Billing.Libs;
 
 namespace Lync_Billing.DB
 {
@@ -12,10 +14,35 @@ namespace Lync_Billing.DB
         string Role { get; set; }
         DateTime AnnouncementDate { get; set; }
 
+        private static DBLib DBRoutines = new DBLib();
+
         public static string GetAnnouncemnet(string role)
         {
+            Announcements announcement = new Announcements();
+            DataTable dt = new DataTable();
 
-            return null;
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.Announcements.TableName),"Role",role);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                announcement = new Announcements();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.Announcements.ID) && row[column.ColumnName] != System.DBNull.Value)
+                        announcement.ID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.Announcements.Role) && row[column.ColumnName] != System.DBNull.Value)
+                        announcement.Role = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.Announcements.Announcement) && row[column.ColumnName] != System.DBNull.Value)
+                        announcement.Announcement = (string)row[column.ColumnName];
+
+                }
+                //announcement.Add(gateway);
+            }
+
+            return announcement.Announcement;
         }
     
     }
