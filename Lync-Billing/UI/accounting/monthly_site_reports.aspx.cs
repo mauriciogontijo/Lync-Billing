f5389a23f329d8cb32160923ec6552d6cbeda379
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-29 04:39:54 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@819 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/UI/accounting/monthly_site_reports.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections.ObjectModel;
+using Ext.Net;
+using System.Web.Script.Serialization;
+using Lync_Billing.DB;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace Lync_Billing.UI.accounting
+{
+    public partial class monthly_site_reports : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //If the user is not loggedin, redirect to Login page.
+            if (Session.Contents["UserData"] == null)
+            {
+                Response.Redirect("~/UI/Login.aspx");
+            }
+            else
+            {
+                UserSession session = new UserSession();
+                session = (UserSession)Session.Contents["UserData"];
+
+                if (!session.IsDeveloper && !session.IsAccountant)
+                {
+                    Response.Redirect("~/UI/User_Dashboard.aspx");
+                }
+            }
+        }
+    }
+}
