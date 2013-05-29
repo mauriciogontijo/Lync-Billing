6c14b246257d28c88977038d136d46aac9cb71e0
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-05-29 12:01:51 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@854 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/Announcements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lync_Billing.DB
+{
+    public class Announcements
+    {
+        int ID { get; set; }
+        string Announcement { get; set; }
+        string Role { get; set; }
+        DateTime AnnouncementDate { get; set; }
+
+        public static string GetAnnouncemnet(string role)
+        {
+
+            return null;
+        }
+    
+    }
+
+    
+}
