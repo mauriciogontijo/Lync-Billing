e4ca98df2d4f429183eed717546f53d613222c4c
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-04 07:16:24 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@969 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/UI/user/manage_address_book.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lync_Billing.UI.user
+{
+    public partial class manage_address_book : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
