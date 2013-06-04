3d5e007a0f53a293de36b290c111d2395c224213
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-04 05:50:44 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@957 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/PhoneBook.cs
+++ Lync-Billing/DB/PhoneBook.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Lync_Billing.Libs;
+using System.Data;
 
 namespace Lync_Billing.DB
 {
@@ -16,10 +17,38 @@ namespace Lync_Billing.DB
 
         private static DBLib DBRoutines = new DBLib();
 
-        public static Dictionary<string, PhoneBook> GetAddressBook(string sipAccount) 
+        public static Dictionary<string, PhoneBook> GetAddressBook(string sipAccount)
         {
             Dictionary<string, PhoneBook> phoneBookEntries = new Dictionary<string, PhoneBook>();
+            DataTable dt = new DataTable();
+            PhoneBook phoneBookEntry;
 
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.PhoneBook.TableName), "SipAccount", sipAccount);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                phoneBookEntry = new PhoneBook();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneBook.ID) && row[column.ColumnName] != System.DBNull.Value)
+                        phoneBookEntry.ID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneBook.SipAccount) && row[column.ColumnName] != System.DBNull.Value)
+                        phoneBookEntry.SipAccount = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneBook.DestinationNumber) && row[column.ColumnName] != System.DBNull.Value)
+                        phoneBookEntry.DestinationNumber = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneBook.Type) && row[column.ColumnName] != System.DBNull.Value)
+                        phoneBookEntry.Type = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.PhoneBook.Name) && row[column.ColumnName] != System.DBNull.Value)
+                        phoneBookEntry.Name = (string)row[column.ColumnName];
+                }
+
+                phoneBookEntries.Add(phoneBookEntry.DestinationNumber,phoneBookEntry);
+            }
 
             return phoneBookEntries;
         }
