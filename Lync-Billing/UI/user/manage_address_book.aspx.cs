67854fe215ae5b9a66390e132e94aacd1ad6c5a2
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-04 12:13:40 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@979 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/user/manage_address_book.aspx.cs
+++ Lync-Billing/UI/user/manage_address_book.aspx.cs
@@ -49,7 +49,25 @@ namespace Lync_Billing.UI.user
 
         protected void PhoneCallsStore_Load(object sender, EventArgs e)
         {
-            
+            UserSession userSession = ((UserSession)Session.Contents["UserData"]);
+
+            wherePart.Add("SourceUserUri", userSession.SipAccount);
+            wherePart.Add("marker_CallTypeID", 1);
+            wherePart.Add("ac_IsInvoiced", "NO");
+
+            columns.Add("SessionIdTime");
+            columns.Add("SessionIdSeq");
+            columns.Add("ResponseTime");
+            columns.Add("SessionEndTime");
+            columns.Add("marker_CallToCountry");
+            columns.Add("DestinationNumberUri");
+            columns.Add("Duration");
+            columns.Add("marker_CallCost");
+            columns.Add("ui_CallType");
+            columns.Add("ui_MarkedOn");
+
+            PhoneCallsStore.DataSource = PhoneCall.GetPhoneCalls(columns, wherePart, 0);
+            PhoneCallsStore.DataBind();
         }
     }
 }
