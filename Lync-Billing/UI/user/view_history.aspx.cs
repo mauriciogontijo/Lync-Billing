891a38df9f329763ded99aaf4b9233e0d1d6d2bd
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-06 12:58:11 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1073 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/user/view_history.aspx.cs
+++ Lync-Billing/UI/user/view_history.aspx.cs
@@ -65,7 +65,7 @@ namespace Lync_Billing.UI.user
 
             wherePart.Add("SourceUserUri", SipAccount);
             wherePart.Add("marker_CallTypeID", 1);
-            //wherePart.Add("ac_IsInvoiced", "NO");
+            //wherePart.Add("ac_IsInvoiced", "YES");
                
             columns.Add("SessionIdTime");
             columns.Add("marker_CallToCountry");
