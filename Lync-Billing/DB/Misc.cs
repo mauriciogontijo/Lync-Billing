ff3253842238c115340bc728fbcf02a827b5aa73
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-10 11:57:08 +0000

    Fix Aligmnet
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1150 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Misc.cs
+++ Lync-Billing/DB/Misc.cs
@@ -8,14 +8,42 @@ namespace Lync_Billing.DB
 {
     public class Misc
     {
-        public static void Message(string msg)
+        public static void Message(string title, string msg, string type)
         {
-            Notification.Show(new NotificationConfig
+            NotificationConfig notificationConfig = new NotificationConfig();
+
+            NotificationAlignConfig AlignCfg = new NotificationAlignConfig
             {
-                Title = "Title",
-                Icon = Icon.Information,
-                Html = msg
-            });
+                ElementAnchor = AnchorPoint.TopRight,
+                TargetAnchor = AnchorPoint.TopRight,
+                OffsetX = -5,
+                OffsetY = 35
+            };
+
+            notificationConfig.Title = title;
+            notificationConfig.Html = msg;
+
+            notificationConfig.AlignCfg = AlignCfg;
+
+            if (type == "success") 
+                notificationConfig.Icon = Icon.Accept;
+            else if(type == "info")
+                notificationConfig.Icon = Icon.Information;
+            else if (type == "warning")
+            {
+
+                notificationConfig.Icon = Icon.AsteriskYellow;
+            }
+            else if (type == "error")
+            {
+                notificationConfig.Icon = Icon.Error;
+            }
+
+
+
+                        
+
+            Notification.Show(notificationConfig);
         }
     }
 }
