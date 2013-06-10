227e79b101382336aada8305b3829226ab1bcb94
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-10 11:31:21 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1146 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/Misc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ext.Net;
+
+namespace Lync_Billing.DB
+{
+    public class Misc
+    {
+        public static void Message(string msg)
+        {
+            Notification.Show(new NotificationConfig
+            {
+                Title = "Title",
+                Icon = Icon.Information,
+                Html = msg
+            });
+        }
+    }
+}
