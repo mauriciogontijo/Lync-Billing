4de4f4856d4196a2ed4c24c41a5e9f86a0176660
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-11 09:17:16 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1162 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/Libs/Dispatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+using Lync_Billing.DB;
+
+namespace Lync_Billing.Libs
+{
+    public class Dispatcher
+    {
+
+        public string URL { get; set; }
+        public UserSession USession {get; set;}
+
+        private static bool ValidateUserSession(string sipAccount,UserSession uSession) 
+        {
+            if (uSession != null && sipAccount == uSession.SipAccount)
+                return true;
+            else
+                return false;
+        }
+
+        public string RedirectURL(string sipAccount, UserSession uSession, string url) 
+        {
+            if (ValidateUserSession(sipAccount, uSession) == true)
+                return url;
+            else
+                return string.Empty;
+        }
+
+        
+    }
+}
