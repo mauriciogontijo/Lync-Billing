716d2d8eaa9512060bec92467bb609cab974729a
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-12 11:23:31 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1202 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/MasterPage.Master.designer.cs
+++ Lync-Billing/UI/MasterPage.Master.designer.cs
@@ -31,15 +31,6 @@ namespace Lync_Billing.UI {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// ScriptManager1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.ScriptManager ScriptManager1;
-        
-        /// <summary>
         /// resourceManager control.
         /// </summary>
         /// <remarks>
