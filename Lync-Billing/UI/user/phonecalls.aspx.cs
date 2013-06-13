414891583233aa279c00fde0a91cebbb1f3ed909
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-13 09:04:37 +0000

    Fix toolTip Issue for the numbers in address book
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1215 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/user/phonecalls.aspx.cs
+++ Lync-Billing/UI/user/phonecalls.aspx.cs
@@ -18,7 +18,7 @@ namespace Lync_Billing.UI.user
     public partial class phonecalls : System.Web.UI.Page
     {
         public Dictionary<string, object> wherePart = new Dictionary<string, object>();
-        public Dictionary<string, PhoneBook> phoneBookEntries;
+        private static Dictionary<string, PhoneBook> phoneBookEntries = new Dictionary<string,PhoneBook>();
         public List<string> columns = new List<string>();
         public List<PhoneCall> phoneCalls;
         List<PhoneCall> AutoMarkedPhoneCalls = new List<PhoneCall>();
@@ -35,10 +35,8 @@ namespace Lync_Billing.UI.user
                 string url = @"~/UI/session/login.aspx?redirect_to=" + redirect_to;
                 Response.Redirect(url);
             }
-
             sipAccount = ((UserSession)HttpContext.Current.Session.Contents["UserData"]).SipAccount;
             phoneBookEntries = PhoneBook.GetAddressBook(sipAccount);
-
         }
       
         protected void AssignBusiness(object sender, DirectEventArgs e)
@@ -133,75 +131,18 @@ namespace Lync_Billing.UI.user
         protected void PhoneCallsStore_ReadData(object sender, StoreReadDataEventArgs e)
         {
             this.e = e;
-            //this.PhoneCallsStore.DataSource = PhoneCall.GetPhoneCalls(columns, wherePart, 0);
             this.PhoneCallsStore.DataBind();
         }
-
-        protected void PhoneCallsStore_Load(object sender, EventArgs e)
-        {
-
-            
-            UserSession userSession = ((UserSession)Session.Contents["UserData"]);
-
-            wherePart.Add("SourceUserUri", userSession.SipAccount);
-            wherePart.Add("marker_CallTypeID", 1);
-            wherePart.Add("ac_IsInvoiced", "NO");
-
-            columns.Add("SessionIdTime");
-            columns.Add("SessionIdSeq");
-            columns.Add("ResponseTime");
-            columns.Add("SessionEndTime");
-            columns.Add("marker_CallToCountry");
-            columns.Add("DestinationNumberUri");
-            columns.Add("Duration");
-            columns.Add("marker_CallCost");
-            columns.Add("ui_CallType");
-            columns.Add("ui_MarkedOn");
-
-            phoneCalls = PhoneCall.GetPhoneCalls(columns, wherePart, 0);
-
-            PhoneBook phoneBookentry;
-
-            PhoneCallsStore.DataSource = phoneCalls;
-
-            foreach (PhoneCall phoneCall in phoneCalls)
-            {
-                phoneBookentry = new PhoneBook();
-                phoneBookentry = GetUserNameByNumber(phoneCall.DestinationNumberUri);
-
-                if (phoneBookentry != null)
-                {
-                    phoneCall.PhoneBookName = phoneBookentry.Name;
-
-                    if (phoneCall.UI_CallType == null)
-                    {
-                        phoneCall.UI_CallType = phoneBookentry.Type;
-                        phoneCall.UI_MarkedOn = DateTime.Now;
-                        AutoMarkedPhoneCalls.Add(phoneCall);
-                    }
-                }
-                else
-                {
-                    phoneCall.PhoneBookName = "N/A";
-                }
-
-            }
-            PhoneCallsStore.DataBind();
-
-            if (AutoMarkedPhoneCalls.Count > 0)
-            {
-                foreach (PhoneCall autoMarkedPhonecall in AutoMarkedPhoneCalls)
-                {
-                    PhoneCall.UpdatePhoneCall(autoMarkedPhonecall);
-                }
-            }
-            ManagePhoneCallsGrid.SetLoading(false);
-        }
-      
+  
         private PhoneBook GetUserNameByNumber(string phoneNumber)
         {
-            if (phoneBookEntries.ContainsKey(phoneNumber))
-                return phoneBookEntries[phoneNumber];
+            if (phoneBookEntries.Count > 0)
+            {
+                if (phoneBookEntries.ContainsKey(phoneNumber))
+                    return phoneBookEntries[phoneNumber];
+                else
+                    return null;
+            }
             else
                 return null;
         }
@@ -244,6 +185,29 @@ namespace Lync_Billing.UI.user
             if (start >= 0 && limit > 0)
                 result = result.Skip(start).Take(limit);
 
+            PhoneBook phoneBookentry;
+
+            foreach (PhoneCall phoneCall in result)
+            {
+                phoneBookentry = new PhoneBook();
+                phoneBookentry = GetUserNameByNumber(phoneCall.DestinationNumberUri);
+
+                if (phoneBookentry != null)
+                {
+                    phoneCall.PhoneBookName = phoneBookentry.Name;
+
+                    if (phoneCall.UI_CallType == null)
+                    {
+                        phoneCall.UI_CallType = phoneBookentry.Type;
+                        phoneCall.UI_MarkedOn = DateTime.Now;
+                        AutoMarkedPhoneCalls.Add(phoneCall);
+                    }
+                }
+                else
+                {
+                    phoneCall.PhoneBookName = "N/A";
+                }
+            }
             count = phoneCalls.Count();
 
             return result.ToList();
