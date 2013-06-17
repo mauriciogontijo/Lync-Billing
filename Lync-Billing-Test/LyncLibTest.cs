da06fef247f28d496e9bc473fc75de086155cce9
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-17 06:10:29 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1271 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing-Test/LyncLibTest.cs
+++ Lync-Billing-Test/LyncLibTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lync_Billing.Libs;
+using Lync_Billing.DB;
 
 namespace Lync_Billing_Test
 {
@@ -7,8 +10,26 @@ namespace Lync_Billing_Test
     public class LyncLibTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void  DB_SELECT()
         {
+            DBLib dbRoutines = new DBLib();
+            
+            string tableName = string.Empty;
+            string whereField = string.Empty;
+            string whereValue = string.Empty;
+
+
+            DataTable dt = new DataTable();
+
+            tableName = Enums.GetDescription(Enums.PhoneCalls.TableName);
+            whereField = Enums.GetDescription(Enums.PhoneCalls.SourceUserUri);
+            whereValue = @"SGhaida@ccc.gr";
+            
+
+           dt = dbRoutines.SELECT(tableName,whereField,whereValue);
+
+           Assert.IsNotNull(dt);
+
         }
     }
 }
