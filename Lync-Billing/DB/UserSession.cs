e943e74e4b455624e954c592e8750a15aa8650c9
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-18 09:50:36 +0000

    Remove History
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1322 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/UserSession.cs
+++ Lync-Billing/DB/UserSession.cs
@@ -20,7 +20,6 @@ namespace Lync_Billing.DB
 
         public List<UserRole> Roles { set; get; }
         public Dictionary<string, string> ClientData { set; get; }
-        public List<PhoneCall> PhoneCallsHistory { set; get; }
         public List<PhoneCall> UnmarkedPhoneCalls { set; get; }
         public List<PhoneCall> MarkedPhoneCalls { set; get; }
         public string MarkedPhoneCallsPerPage { set; get; }
