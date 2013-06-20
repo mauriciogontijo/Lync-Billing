8ea85157657ff3507548160fec890cd899317c9a
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-20 08:04:35 +0000

    Added the index.aspx to the root address of the project. This is an empty page which in it's Page_Load checks the current session and determines to either redirect the flow to Login page or the User Dashboard page.
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1371 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/index.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lync_Billing
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Session != null && HttpContext.Current.Session.Contents["UserData"] != null)
+            {
+                Response.Redirect("~/ui/user/dashboard.aspx");
+            }
+            else
+            {
+                Response.Redirect("~/ui/session/login.aspx");
+            }
+        }
+    }
+}
