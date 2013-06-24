0aa23811752b35126a8fd3355d3206487898ba90
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-24 13:53:07 +0000

    Fix Function Paramters
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1424 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/Libs/Statistics.cs
+++ Lync-Billing/Libs/Statistics.cs
@@ -48,14 +48,14 @@ namespace Lync_Billing.Libs
 
          }
 
-        public DataTable USERS_STATS(DateTime startingDate, DateTime endingDate,string siteName) 
+        public DataTable USERS_STATS(DateTime startingDate, int startingYear,int StartingMonth,int endingYear,int endingMonth,string siteName) 
         {
             DataTable dt = new DataTable();
             OleDbDataReader dr;
             string selectQuery = string.Empty;
 
-            selectQuery = string.Format("SELECT * FROM [dbo].[fnc_Chargable_Calls_By_User] ('{0}','{1}','{2}')",
-                startingDate, endingDate, siteName);
+            selectQuery = string.Format("SELECT * FROM [dbo].[fnc_Chargable_Calls_By_User] ('{0}') WHERE (Year BETWEEN {1} AND {2}) AND (Month BETWEEN {3} AND {4})",
+                siteName, startingYear, endingYear, StartingMonth, endingMonth);
 
             OleDbConnection conn = DBInitializeConnection(ConnectionString_Lync);
             OleDbCommand comm = new OleDbCommand(selectQuery, conn);
