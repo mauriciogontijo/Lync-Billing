3d8233e8f4f4de197ee06d86bfdff4c2fb7599db
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-27 11:56:19 +0000

    Get Gateways
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1499 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Gateway.cs
+++ Lync-Billing/DB/Gateway.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using Lync_Billing.Libs;
 
 namespace Lync_Billing.DB
 {
@@ -9,6 +11,33 @@ namespace Lync_Billing.DB
     {
         public int GatewayId { get; set; }
         public string GatewayName { get; set; }
+
+        private static DBLib DBRoutines = new DBLib();
+
+        public static List<Gateway> GetGateways()
+        {
+            Gateway gateway;
+            DataTable dt = new DataTable();
+            List<Gateway> gateways = new List<Gateway>();
+
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.Gateways.TableName));
+
+            foreach (DataRow row in dt.Rows)
+            {
+                gateway = new Gateway();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.Gateways.GatewayId) && row[column.ColumnName] != System.DBNull.Value)
+                        gateway.GatewayId = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.Gateways.GatewayName) && row[column.ColumnName] != System.DBNull.Value)
+                        gateway.GatewayName = (string)row[column.ColumnName];
+                }
+                gateways.Add(gateway);
+            }
+            return gateways;
+        }
 
     }
 }
