20eaa59f58c4e5e3929b3fa4ab78bbd91e5b5aed
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-27 18:43:28 +0000

    Implement GetCurrencies
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1514 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Currencies.cs
+++ Lync-Billing/DB/Currencies.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using Lync_Billing.Libs;
 
 namespace Lync_Billing.DB
 {
@@ -11,7 +13,34 @@ namespace Lync_Billing.DB
         string CurrencyName { get; set; }
         string CurrencyISOName { get; set; }
 
+        private static DBLib DBRoutines = new DBLib();
 
+        public static List<Currencies> GetCurrencies()
+        {
+            Currencies currency;
+            DataTable dt = new DataTable();
+            List<Currencies> currencies = new List<Currencies>();
 
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.Currencies.TableName));
+
+            foreach (DataRow row in dt.Rows)
+            {
+                currency = new Currencies();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.Currencies.CountryName) && row[column.ColumnName] != System.DBNull.Value)
+                        currency.CountryName = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.Currencies.CurrencyName) && row[column.ColumnName] != System.DBNull.Value)
+                        currency.CurrencyName = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.Currencies.CurrencyISOName) && row[column.ColumnName] != System.DBNull.Value)
+                        currency.CurrencyISOName = (string)row[column.ColumnName];
+                }
+                currencies.Add(currency);
+            }
+            return currencies;
+        }
     }
 }
