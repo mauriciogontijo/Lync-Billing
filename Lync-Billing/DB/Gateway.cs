e45d7da596f587bad5519f06a749c47191cb79ca
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-27 11:37:53 +0000

    Gateway Calls initial code
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1491 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/Gateway.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lync_Billing.DB
+{
+    public class Gateway
+    {
+        public int ID { get; set; }
+        public string GatewayName { get; set; }
+
+    }
+}
