1b89a83f5ea774fecfbbb1ecc30467605d19090a
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-27 18:38:33 +0000

    Add Currencies Class
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1512 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/Currencies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lync_Billing.DB
+{
+    public class Currencies
+    {
+        string CountryName { get; set; }
+        string CurrencyName { get; set; }
+        string CurrencyISOName { get; set; }
+
+
+
+    }
+}
