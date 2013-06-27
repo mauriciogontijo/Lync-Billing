e379dd1b51d3b389ae97936eca6405b5aa85be63
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-27 11:40:12 +0000

    Change fields name
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1493 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/Gateway.cs
+++ Lync-Billing/DB/Gateway.cs
@@ -7,7 +7,7 @@ namespace Lync_Billing.DB
 {
     public class Gateway
     {
-        public int ID { get; set; }
+        public int GatewayId { get; set; }
         public string GatewayName { get; set; }
 
     }
