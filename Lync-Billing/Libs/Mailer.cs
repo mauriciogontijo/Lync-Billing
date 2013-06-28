3d2646261dc89399f4c6be15a0672d8bcf648efe
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-28 09:25:49 +0000

    Fixes
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1536 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/Libs/Mailer.cs
+++ Lync-Billing/Libs/Mailer.cs
@@ -12,15 +12,15 @@ namespace Lync_Billing.Libs
         private SmtpClient client = new SmtpClient();
         private string mailhost = ConfigurationManager.AppSettings["MailHost"];
         
-        public Mailer(string emailAddress,string template)
+        public Mailer(string emailAddress, string templateSubject, string templateBody)
         {
             MailMessage mail = new MailMessage("ebill@ccc.gr", @emailAddress);
             client.Port = 25;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
             client.UseDefaultCredentials = false;
             client.Host = mailhost;
-            mail.Subject = "TBill Notification";
-            mail.Body = template;
+            mail.Subject = templateSubject;
+            mail.Body = templateBody;
             client.Send(mail);
         }
         
