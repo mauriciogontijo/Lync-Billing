2b11b88a768c597504d5066df8f22a60ff84e4bc
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-06-29 13:45:39 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1569 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/Libs/Mailer.cs
+++ Lync-Billing/Libs/Mailer.cs
@@ -11,16 +11,21 @@ namespace Lync_Billing.Libs
     {
         private SmtpClient client = new SmtpClient();
         private string mailhost = ConfigurationManager.AppSettings["MailHost"];
-        
+        private MailAddress replyto = new MailAddress(ConfigurationManager.AppSettings["ReplyTo"]);
+
         public Mailer(string emailAddress, string templateSubject, string templateBody)
         {
             MailMessage mail = new MailMessage("ebill@ccc.gr", @emailAddress);
             client.Port = 25;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
             client.UseDefaultCredentials = false;
+            mail.IsBodyHtml = true;
+
+            //mail.ReplyToList = {replyto};
             client.Host = mailhost;
             mail.Subject = templateSubject;
             mail.Body = templateBody;
+           
             client.Send(mail);
         }
         
