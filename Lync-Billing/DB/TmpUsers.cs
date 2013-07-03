b3750a95ca3c5934fae38004af215fb1bd3549ab
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-07-03 08:50:04 +0000

    Tmp User Class to import all AD users and insert them under unidentified site
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1634 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/TmpUsers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lync_Billing.Libs;
+using System.Data;
+
+namespace Lync_Billing.DB
+{
+    public class TmpUsers
+    {
+        int ID { set; get; }
+        string SipAccount { set; get; }
+
+        private static DBLib DBRoutines = new DBLib();
+        private static AdLib ADRoutine = new AdLib();
+
+        public static List<string> GetAllUsers() 
+        {
+            List<string> users = new List<string>();
+            DataTable dt = new DataTable();
+
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.TmpUsers.TableName));
+
+            TmpUsers user;
+
+            foreach (DataRow row in dt.Rows)
+            {
+                user = new TmpUsers();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.TmpUsers.ID) && row[column.ColumnName] != System.DBNull.Value)
+                        user.ID = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.TmpUsers.SipAccount) && row[column.ColumnName] != System.DBNull.Value)
+                        user.SipAccount = (string)row[column.ColumnName];
+
+                }
+                users.Add(user.SipAccount);
+            }
+            return users;
+        }
+
+        public static void InsertUsers()
+        {
+            List<string> users = GetAllUsers();
+
+            ADUserInfo userInfo;
+
+            foreach (string user in users) 
+            {
+                userInfo = new ADUserInfo();
+                userInfo = ADRoutine.GetUserAttributes(user);
+
+            }
+
+        }
+
+
+    }
+}
