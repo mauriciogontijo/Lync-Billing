2aa3290dd87eac93e0c7e2a3d2e3bd6a50de3213
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-07-11 13:32:32 +0000

    call Stats DB Routines instead of DBLIB routines
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1771 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/DialingPrefixsRates.cs
+++ Lync-Billing/DB/DialingPrefixsRates.cs
@@ -20,6 +20,7 @@ namespace Lync_Billing.DB
         public string TypeOfService { get; set; }
 
         private static DBLib DBRoutines = new DBLib();
+        private static Statistics statRoutines = new Statistics();
 
         public static List<DialingPrefixsRates> GetRates(string ratesTableName)
         {
@@ -27,7 +28,7 @@ namespace Lync_Billing.DB
             DataTable dt = new DataTable();
             DialingPrefixsRates rate;
 
-            dt = DBRoutines.SELECT(ratesTableName);
+            dt = statRoutines.RATESTABLE_VIEW_PER_GATEWAY(ratesTableName);
 
             foreach (DataRow row in dt.Rows)
             {
@@ -73,7 +74,7 @@ namespace Lync_Billing.DB
             DataTable dt = new DataTable();
             DialingPrefixsRates rate;
 
-            dt = dt = DBRoutines.SELECT(ratesTableName, Enums.GetDescription(Enums.ActualRates.DialingPrefix), dialingPrefix);
+            dt = statRoutines.RATESTABLE_VIEW_PER_GATEWAY(ratesTableName,Enums.GetDescription(Enums.ActualRates.DialingPrefix),dialingPrefix);
 
             foreach (DataRow row in dt.Rows)
             {
@@ -121,5 +122,61 @@ namespace Lync_Billing.DB
             }
             return rates;
         }
+
+        public static List<DialingPrefixsRates> GetRates(string ratesTableName, string countryCode)
+        {
+            List<DialingPrefixsRates> rates = new List<DialingPrefixsRates>();
+            DataTable dt = new DataTable();
+            DialingPrefixsRates rate;
+
+            dt = statRoutines.RATESTABLE_VIEW_PER_GATEWAY(ratesTableName, Enums.GetDescription(Enums.ActualRates.CountryName), countryCode);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                rate = new DialingPrefixsRates();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.RateID) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.RateID = (Int64)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.DialingPrefix) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.DialingPrefix = (Int64)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.Rate) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.Rate = (decimal)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.RateID) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.RateID = (Int64)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.DialingPrefix) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.DialingPrefix = (Int64)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.Rate) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.Rate = (decimal)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.CountryName) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.CountryName = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.TwoDigitsCountryCode) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.TwoDigitsCountryCode = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.ThreeDigitsCountryCode) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.ThreeDigitsCountryCode = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.City) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.City = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.Provider) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.Provider = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.ActualRates.TypeOfService) && row[column.ColumnName] != System.DBNull.Value)
+                        rate.TypeOfService = (string)row[column.ColumnName];
+                }
+                rates.Add(rate);
+            }
+            return rates;
+        }
+    
     }
 }
