4629161de88e30ba3c86d360603a3e9d64562b68
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-07-22 07:07:51 +0000

    Classes Wrappers with XML Descriptors
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1888 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/Wrappers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Lync_Billing.DB
+{
+
+    [XmlRoot("Documnet")]
+    public class PhoneCallsWrapper 
+    {
+        [XmlArray("records"), XmlArrayItem(typeof(PhoneCall), ElementName = "record")]
+        public List<PhoneCall> Records { get; set; }
+    }
+}
