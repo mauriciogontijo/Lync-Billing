c1b3a68d80c8c4c8be34a67247c9464ebf90e06c
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-07-22 11:31:36 +0000

    Broken
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1907 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/Libs/PDFLib.cs
+++ Lync-Billing/Libs/PDFLib.cs
@@ -6,15 +6,16 @@ using iTextSharp.text.html.simpleparser;
 using iTextSharp.text.pdf;
 using System.Data;
 using System.Web;
+using Lync_Billing.DB;
 
 namespace Lync_Billing.Libs
 {
-    public class PDFLib 
+    public class PDFLib
     {
-        public static void CreatePDF(DataTable dt) 
+        public static void CreatePDF(DataTable dt)
         {
             Document document = new Document();
-            
+
             string path = HttpRuntime.AppDomainAppPath;
 
             PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(path.ToString() + @"\Exported.pdf", FileMode.Create));
@@ -23,8 +24,8 @@ namespace Lync_Billing.Libs
 
             PdfPTable table = new PdfPTable(dt.Columns.Count);
             PdfPRow row = null;
-            float[] widths = new float[] { 4f, 4f, 4f, 4f };
 
+            //float[] widths = new float[] { 4f, 4f, 4f, 4f };
             //table.SetWidths(widths);
 
             table.WidthPercentage = 100;
@@ -36,15 +37,14 @@ namespace Lync_Billing.Libs
 
             foreach (DataColumn c in dt.Columns)
             {
-
-                table.AddCell(new Phrase(c.ColumnName, font5));
+                table.AddCell(new Phrase(PDFDefinitions.GetDescription((PDFDefinitions.PDF)c.ColumnName), font5));
             }
 
             foreach (DataRow r in dt.Rows)
             {
-                foreach(DataColumn colum in dt.Columns)
+                foreach (DataColumn colum in dt.Columns)
                 {
-                        table.AddCell(new Phrase(r[colum.ColumnName].ToString(),font5));
+                    table.AddCell(new Phrase(r[colum.ColumnName].ToString(), font5));
                 }
             } document.Add(table);
             document.Close();
