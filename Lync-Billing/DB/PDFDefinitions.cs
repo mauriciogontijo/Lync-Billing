602e292d6e16d4b997f7d8c2e67eeadf99b31f2d
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-07-22 11:40:53 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1909 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/PDFDefinitions.cs
+++ Lync-Billing/DB/PDFDefinitions.cs
@@ -62,5 +62,21 @@ namespace Lync_Billing.DB
                 return value.ToString();
         }
 
+        public static T GetDescription<T>(string description)
+        {
+            MemberInfo[] fis = typeof(T).GetFields();
+
+            foreach (var fi in fis)
+            {
+                DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attributes != null && attributes.Length > 0 && attributes[0].Description == description)
+                    return (T)Enum.Parse(typeof(T), fi.Name);
+            }
+
+            throw new Exception("Not found");
+        }
+
+
     }
 }
