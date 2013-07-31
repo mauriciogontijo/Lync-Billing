35ad7e4f2587aeecf37e3c90e8447ccd6b74ef95
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-07-31 08:00:04 +0000

    Added special-puropose function for the Accounting Detailed Report Exports!
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@1990 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/Libs/PDFLib.cs
+++ Lync-Billing/Libs/PDFLib.cs
@@ -14,6 +14,8 @@ namespace Lync_Billing.Libs
 {
     public class PDFLib 
     {
+        public static IElement titleParagraph { get; set; }
+
         private static Font titleFont = FontFactory.GetFont("Arial", 20, Font.BOLD);
         private static Font subTitleFont = FontFactory.GetFont("Arial", 16, Font.BOLD);
         private static Font headerCommentsFont = FontFactory.GetFont("Arial", 9, Font.ITALIC);
@@ -21,8 +23,6 @@ namespace Lync_Billing.Libs
         private static Font endingMessageFont = FontFactory.GetFont("Arial", 10, Font.ITALIC);
         private static Font bodyFont = FontFactory.GetFont("Arial", 12, Font.NORMAL);
         private static Font bodyFontSmall = FontFactory.GetFont("Arial", 10, Font.NORMAL);
-
-        
 
         public static Document InitializePDFDocument(HttpResponse response)
         {
@@ -259,121 +259,7 @@ namespace Lync_Billing.Libs
             return document;
         }
 
-        public static Document AddCombinedPDFTablesContents(ref Document document, DataTable dt, int[] pdfReportColumnsWidths, string handleName, List<string> handles, List<string> pdfReportColumnScheme, string siteName = "")
-        {
-            DataRow[] selectedDataRows;
-            string selectExpression = string.Empty;
-            string pageTitleText = string.Empty;
-            string cellText = string.Empty;
-            Paragraph pageTitleParagraph;
-            ADUserInfo userInfo = new ADUserInfo();
-
-            //Exit the function in case the handles array is empty or the pdfReportColumnScheme is either empty or it's size exceeds the DataTable's Columns number.
-            if (handles == null || handles.Count == 0 || pdfReportColumnScheme == null || pdfReportColumnScheme.Count == 0 || pdfReportColumnScheme.Count > dt.Columns.Count)
-            {
-                return document;
-            }
-            else
-            {
-                //start by sorting the handles array.
-                handles.Sort();
-
-                //Get the users information in case we are dealing with a Users Related Data Table.
-                Dictionary<string, Dictionary<string, string>> usersInformation = new Dictionary<string, Dictionary<string, string>>();
-                if (handleName == "SourceUserUri")
-                {
-                    //List<Users> usersList = Users.GetUsers(siteName);
-                    Dictionary<string, string> info;
-                    List<string> columns = new List<string>() { "AD_UserID", "SipAccount", "AD_DisplayName", "AD_PhysicalDeliveryOfficeName" };
-                    Dictionary<string, object> where = new Dictionary<string, object>() { { "AD_PhysicalDeliveryOfficeName", siteName } };
-                    List<Users> usersList = Users.GetUsers(columns, where, 0);
-
-                    foreach(Users user in usersList)
-                    {
-                        info = new Dictionary<string, string>() { 
-                            {"FullName", user.FullName},
-                            {"ID", user.EmployeeID.ToString()}
-                        };
-
-                        usersInformation.Add(user.SipAccount.ToLower(), info);
-                    }
-                }
-
-                //Begin the construction of the document.
-                foreach (string handleItem in handles)
-                {
-                    PdfPTable pdfTable = InitializePDFTable(pdfReportColumnScheme.Count, pdfReportColumnsWidths);
-                    document.NewPage();
-
-                    if (handleName == "SourceUserUri")
-                        pageTitleText = usersInformation[handleItem.ToLower()]["FullName"] + " [Group Number: " + usersInformation[handleItem.ToLower()]["ID"] + "]";
-                    else
-                        pageTitleText = handleItem;
-
-                    pageTitleParagraph = new Paragraph(pageTitleText, subTitleFont);
-                    pageTitleParagraph.SpacingAfter = 20;
-                    document.Add(pageTitleParagraph);
-
-                    //Select the rows that are associated to the supplied handles
-                    selectExpression = handleName + " = '" + handleItem + "'";
-                    selectedDataRows = dt.Select(selectExpression);
-
-                    //Print the report table columns headers
-                    foreach (string column in pdfReportColumnScheme)
-                    {
-                        if (dt.Columns.Contains(column))
-                        {
-                            pdfTable.AddCell(new Phrase(PDFDefinitions.GetDescription(column), boldTableFont));
-                        }
-                    }
-
-                    //Bind the data cells to the respective columns
-                    foreach (DataRow r in selectedDataRows)
-                    {
-                        foreach (string column in pdfReportColumnScheme)
-                        {
-                            if (dt.Columns.Contains(column))
-                            {
-                                //Declare the pdfTable cell and fill it.
-                                PdfPCell entryCell;
-
-                                //Check if the cell being processed in not  empty nor null.
-                                cellText = r[column].ToString();
-                                if (string.IsNullOrEmpty(cellText))
-                                    cellText = "N/A";
-
-                                //Format the cell text if it's the case of Duration
-                                if (PDFDefinitions.GetDescription(column) == "Duration" && cellText != "N/A")
-                                {
-                                    entryCell = new PdfPCell(new Phrase(Misc.ConvertSecondsToReadable(Convert.ToInt32(cellText)), bodyFontSmall));
-                                }
-                                else
-                                {
-                                    entryCell = new PdfPCell(new Phrase(cellText, bodyFontSmall));
-                                }
-
-                                //Set the cell padding, border configurations and then add it to the the pdfTable
-                                entryCell.Border = Rectangle.BOTTOM_BORDER | Rectangle.TOP_BORDER;
-                                entryCell.PaddingTop = 5;
-                                entryCell.PaddingBottom = 5;
-                                entryCell.PaddingLeft = 2;
-                                entryCell.PaddingRight = 2;
-                                pdfTable.AddCell(entryCell);
-                            }
-                        }
-                    }
-
-                    // Add the Paragraph object to the document
-                    document.Add(pdfTable);
-                    selectExpression = string.Empty;
-                    Array.Clear(selectedDataRows, 0, selectedDataRows.Length);
-                }
-            }
-            return document;
-        }
-
-
-        public static Document AddCombinedPDFTablesContents(ref Document document, DataTable dt, int[] pdfReportColumnsWidths, string handleName, Dictionary<string, Dictionary<string, object>> UsersCollection, List<string> pdfReportColumnScheme, string siteName = "")
+        public static Document AddAccountingDetailedReportBody(ref Document document, DataTable dt, int[] pdfReportColumnsWidths, string handleName, Dictionary<string, Dictionary<string, object>> UsersCollection, List<string> pdfReportColumnScheme, Dictionary<string, object> extraParams = null)
         {
             DataRow[] selectedDataRows;
             string selectExpression = string.Empty;
@@ -472,6 +358,7 @@ namespace Lync_Billing.Libs
                     // Add the Paragraph object to the document
                     document.Add(pdfTable);
 
+                    AddAccountingDetailedReportTotalsRow(ref document, sipAccount, extraParams);
 
                     selectExpression = string.Empty;
                     Array.Clear(selectedDataRows, 0, selectedDataRows.Length);
@@ -493,9 +380,7 @@ namespace Lync_Billing.Libs
             pdfTable.WidthPercentage = 100;
             if (widths.Length > 0 && widths.Length == dt.Columns.Count)
                 pdfTable.SetWidths(widths);
-            //else
-            //    pdfTable.SetWidths(new int[] { 7, 4, 7, 4, 4 });
-
+            
             foreach(DataColumn column in dt.Columns)
             {
                 if (dt.Columns[0].ColumnName == column.ColumnName)
@@ -516,12 +401,68 @@ namespace Lync_Billing.Libs
             return document;
         }
 
+
+        private static Document AddAccountingDetailedReportTotalsRow(ref Document document, string sipAccount, Dictionary<string, object> extraParams)
+        {
+            PdfPTable pdfTable = new PdfPTable(5);
+            pdfTable.HorizontalAlignment = 0;
+            pdfTable.DefaultCell.Border = 0;
+            pdfTable.DefaultCell.PaddingBottom = 5;
+            pdfTable.DefaultCell.PaddingTop = 5;
+            pdfTable.DefaultCell.PaddingLeft = 2;
+            pdfTable.DefaultCell.PaddingRight = 2;
+            pdfTable.WidthPercentage = 100;
+
+            int[] widths = new int[] { 8, 5, 8, 8, 8 };
+            pdfTable.SetWidths(widths);
+            
+            int year = ((DateTime)extraParams["StartDate"]).Year;
+            int startMonth = ((DateTime)extraParams["StartDate"]).Month;
+            int endMonth = ((DateTime)extraParams["EndDate"]).Month;
+
+            UsersCallsSummary userSummary = UsersCallsSummary.GetUserCallsSummary(sipAccount, year, startMonth, endMonth);
+
+            //TOTALS HEADERS
+            pdfTable.AddCell(new Phrase("Totals", boldTableFont));
+            pdfTable.AddCell(new Phrase(string.Empty, boldTableFont));
+            pdfTable.AddCell(new Phrase("Call Type", boldTableFont));
+            pdfTable.AddCell(new Phrase("Cost", boldTableFont));
+            pdfTable.AddCell(new Phrase("Duration", boldTableFont));
+            pdfTable.CompleteRow();
+
+            //Total Costs & Durations
+            //Personal Calls Totals
+            pdfTable.AddCell(new Phrase(string.Empty, bodyFont));
+            pdfTable.AddCell(new Phrase(string.Empty, bodyFont));
+            pdfTable.AddCell(new Phrase("Personal", bodyFont));
+            pdfTable.AddCell(new Phrase(Decimal.Round(userSummary.PersonalCallsCost, 2).ToString(), bodyFontSmall));
+            pdfTable.AddCell(new Phrase(Misc.ConvertSecondsToReadable(userSummary.PersonalCallsDuration).ToString(), bodyFontSmall));
+            pdfTable.CompleteRow();
+
+            //Business Calls Totals
+            pdfTable.AddCell(new Phrase(string.Empty, bodyFont));
+            pdfTable.AddCell(new Phrase(string.Empty, bodyFont));
+            pdfTable.AddCell(new Phrase("Business", bodyFont));
+            pdfTable.AddCell(new Phrase(Decimal.Round(userSummary.BusinessCallsCost, 2).ToString(), bodyFontSmall));
+            pdfTable.AddCell(new Phrase(Misc.ConvertSecondsToReadable(userSummary.BusinessCallsDuration).ToString(), bodyFontSmall));
+            pdfTable.CompleteRow();
+
+            //Unallocated Calls Totals
+            pdfTable.AddCell(new Phrase(string.Empty, bodyFont));
+            pdfTable.AddCell(new Phrase(string.Empty, bodyFont));
+            pdfTable.AddCell(new Phrase("Unallocated", bodyFont));
+            pdfTable.AddCell(new Phrase(Decimal.Round(userSummary.UnmarkedCallsCost, 2).ToString(), bodyFontSmall));
+            pdfTable.AddCell(new Phrase(Misc.ConvertSecondsToReadable(userSummary.UnmarkedCallsDuration).ToString(), bodyFontSmall));
+            pdfTable.CompleteRow();
+
+            document.Add(pdfTable);
+            return document;
+        }
+
         public static void ClosePDFDocument(ref Document document)
         {
             document.Close();
         }
 
-
-        public static IElement titleParagraph { get; set; }
     }
 }
