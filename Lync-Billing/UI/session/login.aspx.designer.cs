d6fcc58d537a581863bb79766852f4882cf38de4
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-08-01 09:09:01 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2011 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/session/login.aspx.designer.cs
+++ Lync-Billing/UI/session/login.aspx.designer.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Lync_Billing.ui.session {
+namespace Lync_Billing.ui.session { 
     
     
     public partial class login {
