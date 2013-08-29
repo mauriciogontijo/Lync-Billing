f42d2852ca5c04d81b1020738a692be37094bc62
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-08-29 12:22:10 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2054 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/UI/admin/notifications/bills.aspx.cs
+++ Lync-Billing/UI/admin/notifications/bills.aspx.cs
@@ -59,7 +59,7 @@ namespace Lync_Billing.ui.admin.notifications
             {
                 DB.Site tmpSite = new DB.Site();
 
-                if (role.SiteID != 0 && (role.IsSiteAdmin() || role.IsSiteAdmin()))
+                if (role.SiteID != 0 && (role.IsSiteAdmin() || role.IsDeveloper()))
                 {
                     tmpSite.SiteID = role.SiteID;
                     sites.Add(tmpSite);
