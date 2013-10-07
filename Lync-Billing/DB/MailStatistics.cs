3141537ed04228fabdebe9f9a195556dc45dbeba
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-07 06:35:11 +0000

    This is the new MailStatistics class.
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2082 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/DB/MailStatistics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lync_Billing.DB
+{
+    public class MailStatistics
+    {
+        public string EmailAddress { get; set; }
+        public long ReceivedCount { get; set; }
+        public long ReceivedSize { get; set; }
+        public long SentCount { get; set; }
+        public long SentSize { get; set; }
+
+        public static List<MailStatistics> GetMailStatistics()
+        {
+            return null;
+        }
+    }
+}
