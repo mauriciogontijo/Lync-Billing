dd2ce2a49bcba02a16a953ee869750cb21c6e76a
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-14 15:12:56 +0000

    Pools and gateways classes
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2163 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/Gateways.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lync_Backend.Helpers
+{
+    class Gateways
+    {
+        public int GatewayId { get; set; }
+        public string GatewayName { get; set; }
+    }
+}
---
+++ Lync-Backend/Helpers/Pools.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lync_Backend.Helpers
+{
+    class Pools
+    {
+        public int PoolID { set; get; }
+        public string PoolFQDN { set; get; }
+    }
+}
