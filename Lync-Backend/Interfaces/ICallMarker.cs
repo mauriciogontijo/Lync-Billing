9dff77676a674de21cd23fdc169bae1ff025747d
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-14 07:24:00 +0000

    CallMarker Interface
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2141 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Interfaces/ICallMarker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lync_Backend.Interfaces
+{
+    interface ICallMarker
+    {
+        public void MarkCalls(string tableName);
+        public void MarkExclusion(string tableName);
+        public void ApplyRates(string tableName);
+        public void ResetPhoneCallsAttributes(string tableName);
+    }
+}
