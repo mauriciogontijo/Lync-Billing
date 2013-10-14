eadee75d9862d7d285f944a3329ffa7269cd7537
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-14 07:16:23 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2139 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Interfaces/IImporter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lync_Backend.Helpers;
+
+namespace Lync_Backend.Interfaces
+{
+    interface IImporter
+    {
+        public void ImportPhoneCalls(MonitoringServersInfo info);
+        public void ImportGateways(MonitoringServersInfo info);
+        public void ImportPools(MonitoringServersInfo info);
+    }
+}
