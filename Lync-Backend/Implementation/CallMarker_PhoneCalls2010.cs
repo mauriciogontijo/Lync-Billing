f44fb6f056e2b9c82e1067e027797b55f31a0bd8
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-14 14:50:41 +0000

    Call Marker for phonecalls 2010
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2153 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Implementation/CallMarker_PhoneCalls2010.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lync_Backend.Interfaces;
+
+namespace Lync_Backend.Implementation
+{
+    class CallMarker_PhoneCalls2010 : ICallMarker
+    {
+        public void MarkCalls(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkExclusion(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ApplyRates(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetPhoneCallsAttributes(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
