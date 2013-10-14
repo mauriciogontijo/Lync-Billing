2a7d049c669543649f7c0e4a423c1263ecccb6cb
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-14 07:16:14 +0000

    Add DB IMporter interface
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2138 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/UsersImporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lync_Backend.Helpers
+{
+    class UsersImporter
+    {
+        //TODO: Put all the Business logic for importing users and their related information 
+    }
+}
