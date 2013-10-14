f1850c418b19240d7977ac5d2171baa3430d2051
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-14 05:39:49 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2134 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/Enums.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace Lync_Backend.Helpers
+{
+    public static class Enums
+    {
+
+        public enum MonitoringServersInfo
+        {
+            [Description("MonitoringServersInfo")]
+            TableName,
+            [Description("id")]
+            Id,
+            [Description("instanceHostName")]
+            InstanceHostName,
+            [Description("instanceName")]
+            InstanceName,
+            [Description("userName")]
+            Userame,
+            [Description("password")]
+            Password,
+            [Description("PhoneCallsTable")]
+            PhoneCallsTable
+        }
+
+        /// <summary>
+        /// Gets the Name of DB table Field
+        /// </summary>
+        /// <param name="value">Enum Name</param>
+        /// <returns>Field Description</returns>
+        public static string GetDescription(Enum value)
+        {
+            FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] descAttributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (descAttributes != null && descAttributes.Length > 0)
+                return descAttributes[0].Description;
+            else
+                return value.ToString();
+        }
+
+        public static IEnumerable<T> EnumToList<T>()
+        {
+            Type enumType = typeof(T);
+
+            if (enumType.BaseType != typeof(Enum))
+                throw new ArgumentException("T is not of System.Enum Type");
+
+            Array enumValArray = Enum.GetValues(enumType);
+            List<T> enumValList = new List<T>(enumValArray.Length);
+
+            foreach (int val in enumValArray)
+            {
+                enumValList.Add((T)Enum.Parse(enumType, val.ToString()));
+            }
+
+            return enumValList;
+        }
+    }
+}
