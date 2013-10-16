42481b1d31b8ea59ba4fd5d3976ca85af63e7269
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-16 04:50:25 +0000

    Initial class
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2168 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Implementation/Lync2013.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lync_Backend.Interfaces;
+
+namespace Lync_Backend.Implementation
+{
+    class Lync2013 : IDatabaseImporter
+    {
+        public string ConstructConnectionString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ImportPhoneCalls()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ImportGateways()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ImportPools()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
