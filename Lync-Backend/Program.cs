fc15bc14021f0d79c73f450996224c23f90ce7b3
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-16 07:11:43 +0000

    Implemnets Class loader
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2191 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Backend/Program.cs
+++ Lync-Backend/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Lync_Backend;
+using Lync_Backend.Helpers;
+using Lync_Backend.Libs;
+using Lync_Backend.Implementation;
 
 namespace Lync_Backend
 {
@@ -12,8 +15,23 @@ namespace Lync_Backend
         static void Main(string[] args)
         {
             //Interfaces.IDatabaseImporter importer = new Implementation.Lync2010();
-            Interfaces.IDatabaseImporter importer = new Implementation.Lync2013();
-            importer.ImportPhoneCalls();
+
+            Dictionary<string, MonitoringServersInfo> monServersInfo = new Dictionary<string, MonitoringServersInfo>();
+
+            monServersInfo = MonitoringServersInfo.GetMonitoringServersInfo();
+
+            foreach (KeyValuePair<string, MonitoringServersInfo> keyValue in monServersInfo) 
+            {
+                Type type = Type.GetType("Lync_Backend.Implementation." + keyValue.Key);
+
+                string fqdn = typeof(Lync2010).AssemblyQualifiedName;
+
+                //FQN  for Lync2010: Lync_Backend.Implementation.Lync2010, Lync-Backend, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+                  object instance = Activator.CreateInstance(type);
+
+                  ((Interfaces.IDatabaseImporter)instance).ImportGateways();
+
+            }
 
         }
     }
