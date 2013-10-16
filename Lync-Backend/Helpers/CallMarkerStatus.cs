f1812ef7bea646705663169f2d9e9e4ea7ba7b3d
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-16 13:58:06 +0000

    Add Call Marker Status class
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2232 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/CallMarkerStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lync_Backend.Helpers
+{
+    class CallMarkerStatus
+    {
+        public int MarkerId { get; set; }
+        public string PhoneCallsTable { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
