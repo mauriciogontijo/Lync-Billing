c014b620f306b05500ace6949b1f33b205f88976
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-16 13:07:14 +0000

    Add numbering plan helper
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2221 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/NumberingPlan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lync_Backend.Helpers
+{
+    class NumberingPlan
+    {
+        public long Dialing_prefix { get; set; }
+        public string CountryName { get; set; }
+        public string Two_Digits_country_code { get; set; }
+        public string Three_Digits_Country_Code { get; set; }
+        public string City { get; set; }
+        public string Provider { get; set; }
+        public string Type_Of_Service { get; set; }
+        
+        
+    }
+}
