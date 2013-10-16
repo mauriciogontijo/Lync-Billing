4dc5f86b23691a6841f4cf7e3e32920f78e168f7
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-16 14:05:56 +0000

    Implement GetCallMarkerStatus
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2233 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Backend/Helpers/CallMarkerStatus.cs
+++ Lync-Backend/Helpers/CallMarkerStatus.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using Lync_Backend.Libs;
 
 namespace Lync_Backend.Helpers
 {
@@ -11,5 +13,41 @@ namespace Lync_Backend.Helpers
         public int MarkerId { get; set; }
         public string PhoneCallsTable { get; set; }
         public DateTime Timestamp { get; set; }
+
+        private static DBLib DBRoutines = new DBLib();
+
+        public static List<CallMarkerStatus> GetCallMarkerStatus() 
+        {
+            DataTable dt = new DataTable();
+            CallMarkerStatus callMarkerEntryStat;
+
+
+            List<CallMarkerStatus> callMarkingStatus = new List<CallMarkerStatus>();
+
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.CallMarkerStatus.TableName));
+
+            foreach (DataRow row in dt.Rows)
+            {
+                callMarkerEntryStat = new CallMarkerStatus();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.CallMarkerStatus.MarkerId) && row[column.ColumnName] != System.DBNull.Value)
+                        callMarkerEntryStat.MarkerId = (int)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.CallMarkerStatus.PhoneCallsTable) && row[column.ColumnName] != System.DBNull.Value)
+                        callMarkerEntryStat.PhoneCallsTable = (string)row[column.ColumnName];
+
+                    if (column.ColumnName == Enums.GetDescription(Enums.CallMarkerStatus.Timestamp) && row[column.ColumnName] != System.DBNull.Value)
+                        callMarkerEntryStat.Timestamp = (DateTime)row[column.ColumnName];
+                }
+
+                callMarkingStatus.Add(callMarkerEntryStat);
+            }
+
+            return callMarkingStatus;
+        }
+    
+    
     }
 }
