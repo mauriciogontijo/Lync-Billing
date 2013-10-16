ed3ea9c0a13341fde0e8f2615707905da9e53179
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-16 04:58:21 +0000

    Misc Class
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2171 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/Misc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lync_Backend.Helpers
+{
+    public class Misc
+    {
+        public static string ConvertDate(DateTime datetTime)
+        {
+            return datetTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+        }
+    }
+}
