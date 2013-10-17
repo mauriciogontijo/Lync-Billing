92e6ccd709e2a34fd5cf9f8f842aaab86a56ee3d
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-17 07:45:39 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2257 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Backend/Helpers/CallTypes.cs
+++ Lync-Backend/Helpers/CallTypes.cs
@@ -3,18 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lync_Backend.Libs;
+using System.Data;
 
 namespace Lync_Backend.Helpers
 {
-    class CallTypes
+    class CallsTypes
     {
         public int id { get; set; }
         public string CallType { get; set; }
 
+        private static DBLib DBRoutines = new DBLib();
 
-        public static List<CallTypes> GetCallTypes() 
+
+        public static List<CallsTypes> GetCallTypes() 
         {
+            DataTable dt;
 
+            List<CallsTypes> callsTypes = new List<CallsTypes>();
+
+            CallsTypes callType;
+
+            dt = DBRoutines.SELECT("CallTypes");
+
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    callType = new CallsTypes();
+
+                    callType.id = Convert.ToInt32(row["id"]);
+                    callType.CallType = row["CallType"].ToString();
+
+                    callsTypes.Add(callType);
+                }
+            }
+            
+            return callsTypes;
         }
+
     }
 }
