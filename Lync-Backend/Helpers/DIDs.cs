d9925e5606b5bb70faaa9cc4fdd1af860c1c7380
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-17 08:48:15 +0000

    DIDs
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2259 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/DIDs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lync_Backend.Libs;
+
+namespace Lync_Backend.Helpers
+{
+    class DIDs
+    {
+        public int id { get; set; }
+        public string did { get; set; }
+        public string description { get; set; }
+
+        private static DBLib DBRoutines = new DBLib();
+
+
+
+    }
+}
