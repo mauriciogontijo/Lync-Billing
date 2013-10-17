157319fb026bd0edf0ceeaab78da6115b7f1b204
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-17 07:24:47 +0000

    Add Call Types Class
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2255 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/CallTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lync_Backend.Helpers
+{
+    class CallTypes
+    {
+        public int id { get; set; }
+        public string CallType { get; set; }
+
+
+        public static List<CallTypes> GetCallTypes() 
+        {
+
+        }
+    }
+}
