fd47007bc2090e3b478857a0db7e951568df8b11
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-18 07:23:20 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2275 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Backend/Program.cs
+++ Lync-Backend/Program.cs
@@ -32,8 +32,8 @@ namespace Lync_Backend
             //    ((Interfaces.IDatabaseImporter)instance).ImportPhoneCalls();
             //}
 
-            Interfaces.ICallMarker callsMarker = new CallMarker_Lync2013();
-            callsMarker.MarkCalls("PhoneCalls2013");
+            Interfaces.ICallMarker callsMarker = new CallMarker_Lync2010();
+            callsMarker.MarkCalls("PhoneCalls2010");
 
         }
     }
