6727b356ecfc0a12b81554991447eca5340a8cad
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-25 14:58:52 +0000

    Some organization
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2336 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Backend/Implementation/PhoneCalls2010.cs
+++ Lync-Backend/Implementation/PhoneCalls2010.cs
@@ -15,6 +15,8 @@ namespace Lync_Backend.Helpers
             string dstCountry = string.Empty;
             string srcCallType = string.Empty;
             string dstCallType = string.Empty;
+            string srcDIDdsc = string.Empty; 
+            string dstDIDdsc = string.Empty;
 
             if (thisCall.SessionIdTime == "2013-05-31 05:18:50.653")
             {
@@ -37,6 +39,9 @@ namespace Lync_Backend.Helpers
 
             thisCall.Marker_CallToCountry = dstCountry;
 
+            MatchDID(thisCall.SourceNumberUri, out srcDIDdsc);
+            MatchDID(thisCall.DestinationNumberUri, out dstDIDdsc);
+
             //Incoming Call
             if (string.IsNullOrEmpty(thisCall.SourceUserUri) || !Misc.IsValidEmail(thisCall.SourceUserUri))
             {
@@ -56,30 +61,66 @@ namespace Lync_Backend.Helpers
             }
 
             //CHECK if the Source AND destination is Lync Client
-            string srcDIDdsc = string.Empty, dstDIDdsc = string.Empty;
+           
+            //if ((!string.IsNullOrEmpty(srcDIDdsc) && !string.IsNullOrEmpty(dstDIDdsc)))
+            //{
+            //    if (dstDIDdsc == "TOLL-FREE")
+            //    {
+            //        thisCall.marker_CallType = srcDIDdsc + "-TO-" + dstDIDdsc;
+            //        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "TOLL-FREE").id;
 
-            MatchDID(thisCall.SourceNumberUri, out srcDIDdsc);
-            MatchDID(thisCall.DestinationNumberUri, out dstDIDdsc);
+            //        return thisCall;
+            //    }
+            //    else if (dstDIDdsc == "PUSH-TO-TALK-UAE")
+            //    {
+            //        thisCall.marker_CallType = srcDIDdsc + "-TO-" + dstDIDdsc;
+            //        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "PUSH-TO-TALK").id;
 
-            if ((!string.IsNullOrEmpty(srcDIDdsc) && !string.IsNullOrEmpty(dstDIDdsc)))
-            {
+            //        return thisCall;
+            //    }
+            //    else
+            //    {
+            //        thisCall.marker_CallType = srcDIDdsc + "-TO-" + dstDIDdsc;
+            //        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "SITE-TO-SITE").id;
+
+            //        return thisCall;
+            //    }
+            //}
+
+             if (!string.IsNullOrEmpty(dstDIDdsc))
+             {
+                //TODO:  IF source number uri is null check if the user site could be resolved from activedirectoryUsers table
+                //       IF yes put the source site from activedirectoryUsers table instead of the soyrce did site
+               
                 if (dstDIDdsc == "TOLL-FREE")
                 {
-                    thisCall.marker_CallType = srcDIDdsc + "-TO-" + dstDIDdsc;
+                    if(!string.IsNullOrEmpty(srcDIDdsc))
+                        thisCall.marker_CallType = srcDIDdsc + "-TO-" + dstDIDdsc;
+                    else
+                        thisCall.marker_CallType = "UNKNOWN-TO-" + dstDIDdsc;
+                    
                     thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "TOLL-FREE").id;
 
                     return thisCall;
                 }
                 else if (dstDIDdsc == "PUSH-TO-TALK-UAE")
                 {
-                    thisCall.marker_CallType = srcDIDdsc + "-TO-" + dstDIDdsc;
+                    if (!string.IsNullOrEmpty(srcDIDdsc))
+                        thisCall.marker_CallType = srcDIDdsc + "-TO-" + dstDIDdsc;
+                    else
+                        thisCall.marker_CallType = "UNKNOWN-TO-" + dstDIDdsc;
+
                     thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "PUSH-TO-TALK").id;
 
                     return thisCall;
                 }
                 else
                 {
-                    thisCall.marker_CallType = srcDIDdsc + "-TO-" + dstDIDdsc;
+                    if (!string.IsNullOrEmpty(srcDIDdsc))
+                        thisCall.marker_CallType = srcDIDdsc + "-TO-" + dstDIDdsc;
+                    else
+                        thisCall.marker_CallType = "UNKNOWN-TO-" + dstDIDdsc;
+
                     thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "SITE-TO-SITE").id;
 
                     return thisCall;
@@ -96,86 +137,183 @@ namespace Lync_Backend.Helpers
             }
 
             // MARK NATIONAL INTERNATIONAL FIXED/MOBILE
-            if (!string.IsNullOrEmpty(thisCall.ToGateway) && !string.IsNullOrEmpty(thisCall.DestinationNumberUri) && thisCall.DestinationNumberUri.StartsWith("+"))
+            //if (!string.IsNullOrEmpty(thisCall.ToGateway) && 
+            //    !string.IsNullOrEmpty(thisCall.DestinationNumberUri) && 
+            //    thisCall.DestinationNumberUri.StartsWith("+"))
+            //{
+            //    //HANDLE THE PHONECALLS-EXCEPTIONS HERE
+            //    if (ListOfUserNumbersExceptions.Contains(thisCall.DestinationNumberUri) || ListOfUserUrisExceptions.Contains(thisCall.SourceUserUri))
+            //    {
+            //        thisCall.marker_CallType = "EXCLUDED";
+            //        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+
+            //        return thisCall;
+            //    }
+
+            //    // CHECK FOR THE SOURCE AND DESTINATION COUNTRIES
+            //    if (srcCountry == dstCountry)
+            //    {
+            //        if (!string.IsNullOrEmpty(dstDIDdsc))
+            //        {
+            //            thisCall.marker_CallType = "TO-" + dstDIDdsc;
+            //            thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "SITE-TO-SITE").id;
+
+            //            return thisCall;
+            //        }
+
+            //        if (dstCallType == "fixedline")
+            //        {
+            //            thisCall.marker_CallType = "NATIONAL-FIXEDLINE";
+            //            thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+
+            //            return thisCall;
+            //        }
+            //        else if (dstCallType == "gsm")
+            //        {
+            //            thisCall.marker_CallType = "NATIONAL-MOBILE";
+            //            thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+
+            //            return thisCall;
+            //        }
+            //        else
+            //        {
+            //            thisCall.marker_CallType = "NATIONAL-FIXEDLINE";
+            //            thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+
+            //            return thisCall;
+            //        }
+            //    }
+
+            //    else
+            //    {
+            //        if (!string.IsNullOrEmpty(dstDIDdsc))
+            //        {
+            //            thisCall.marker_CallType = "TO-" + dstDIDdsc;
+            //            thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "SITE-TO-SITE").id;
+
+            //            return thisCall;
+            //        }
+
+            //        if (dstCallType == "fixedline")
+            //        {
+            //            thisCall.marker_CallType = "INTERNATIONAL-FIXEDLINE";
+            //            thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+
+            //            return thisCall;
+            //        }
+            //        else if (dstCallType == "gsm")
+            //        {
+            //            thisCall.marker_CallType = "INTERNATIONAL-MOBILE";
+            //            thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+
+            //            return thisCall;
+            //        }
+            //        else
+            //        {
+            //            thisCall.marker_CallType = "INTERNATIONAL-FIXEDLINE";
+            //            thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+
+            //            return thisCall;
+            //        }
+            //    }
+            //}
+
+             if (srcCountry == dstCountry)
             {
-                //HANDLE THE PHONECALLS-EXCEPTIONS HERE
-                if (ListOfUserNumbersExceptions.Contains(thisCall.DestinationNumberUri) || ListOfUserUrisExceptions.Contains(thisCall.SourceUserUri))
+                if (!string.IsNullOrEmpty(dstDIDdsc))
                 {
-                    thisCall.marker_CallType = "EXCLUDED";
-                    thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+                    if (!string.IsNullOrEmpty(srcDIDdsc))
+                        thisCall.marker_CallType = srcDIDdsc + "-TO-" + dstDIDdsc;
+                    else
+                        thisCall.marker_CallType = "TO-" + dstDIDdsc;
+
+                    thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "SITE-TO-SITE").id;
 
                     return thisCall;
                 }
 
-                // CHECK FOR THE SOURCE AND DESTINATION COUNTRIES
-                if (srcCountry == dstCountry)
+                if (dstCallType == "fixedline")
                 {
-                    if (!string.IsNullOrEmpty(dstDIDdsc))
-                    {
-                        thisCall.marker_CallType = "TO-" + dstDIDdsc;
-                        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "SITE-TO-SITE").id;
+                    thisCall.marker_CallType = "NATIONAL-FIXEDLINE";
+                    thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
 
-                        return thisCall;
-                    }
-
-                    if (dstCallType == "fixedline")
-                    {
-                        thisCall.marker_CallType = "NATIONAL-FIXEDLINE";
-                        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
-
-                        return thisCall;
-                    }
-                    else if (dstCallType == "gsm")
-                    {
-                        thisCall.marker_CallType = "NATIONAL-MOBILE";
-                        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
-
-                        return thisCall;
-                    }
-                    else
-                    {
-                        thisCall.marker_CallType = "NATIONAL-FIXEDLINE";
-                        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
-
-                        return thisCall;
-                    }
+                    return thisCall;
                 }
+                else if (dstCallType == "gsm")
+                {
+                    thisCall.marker_CallType = "NATIONAL-MOBILE";
+                    thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
 
+                    return thisCall;
+                }
                 else
                 {
-                    if (!string.IsNullOrEmpty(dstDIDdsc))
-                    {
-                        thisCall.marker_CallType = "TO-" + dstDIDdsc;
-                        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "SITE-TO-SITE").id;
+                    thisCall.marker_CallType = "NATIONAL-FIXEDLINE";
+                    thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
 
-                        return thisCall;
-                    }
-
-                    if (dstCallType == "fixedline")
-                    {
-                        thisCall.marker_CallType = "INTERNATIONAL-FIXEDLINE";
-                        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
-
-                        return thisCall;
-                    }
-                    else if (dstCallType == "gsm")
-                    {
-                        thisCall.marker_CallType = "INTERNATIONAL-MOBILE";
-                        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
-
-                        return thisCall;
-                    }
-                    else
-                    {
-                        thisCall.marker_CallType = "INTERNATIONAL-FIXEDLINE";
-                        thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
-
-                        return thisCall;
-                    }
+                    return thisCall;
                 }
             }
 
-            //Unable to mark
+            else
+            {
+                if (!string.IsNullOrEmpty(dstDIDdsc))
+                {
+                    if (!string.IsNullOrEmpty(srcDIDdsc))
+                        thisCall.marker_CallType = srcDIDdsc + "-TO-" + dstDIDdsc;
+                    else
+                        thisCall.marker_CallType = "TO-" + dstDIDdsc;
+
+                    thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == "SITE-TO-SITE").id;
+
+                    return thisCall;
+                }
+
+                if (dstCallType == "fixedline")
+                {
+                    thisCall.marker_CallType = "INTERNATIONAL-FIXEDLINE";
+                    thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+
+                    return thisCall;
+                }
+                else if (dstCallType == "gsm")
+                {
+                    thisCall.marker_CallType = "INTERNATIONAL-MOBILE";
+                    thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+
+                    return thisCall;
+                }
+                else
+                {
+                    thisCall.marker_CallType = "INTERNATIONAL-FIXEDLINE";
+                    thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+
+                    return thisCall;
+                }
+            }
+            
+
+           
+            // Handle the sourceNumber uri sip account and the destination uri is a sip account also 
+            // For the calls which doesnt have source number uri or destination number uri to bable to identify which site
+            if (!string.IsNullOrEmpty(thisCall.SourceUserUri) && 
+                !string.IsNullOrEmpty(thisCall.DestinationUserUri) && 
+                Misc.IsValidEmail(thisCall.DestinationUserUri)) 
+            {
+                if (Misc.IsIMEmail(thisCall.DestinationUserUri))
+                {
+                    thisCall.marker_CallType = "LYNC-TO-IM";
+                    thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+                }
+                else
+                {
+                    thisCall.marker_CallType = "LYNC-TO-LYNC";
+                    thisCall.Marker_CallTypeID = callTypes.Find(type => type.CallType == thisCall.marker_CallType).id;
+                }
+
+                return thisCall;
+            }
+
             thisCall.marker_CallType = "N/A";
             thisCall.Marker_CallTypeID = 0;
 
