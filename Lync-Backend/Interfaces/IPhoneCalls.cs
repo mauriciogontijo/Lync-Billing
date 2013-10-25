bd35c5b8089721361ce864c15eda31c52d735bf9
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-25 10:55:15 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2326 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Interfaces/IPhoneCalls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lync_Backend.Helpers;
+
+namespace Lync_Backend.Interfaces
+{
+    interface IPhoneCalls
+    {
+        PhoneCalls SetCallType(PhoneCalls thisCall);
+    }
+}
