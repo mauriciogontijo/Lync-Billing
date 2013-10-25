07afa1d47d266aabce38bfc230df2501111510d4
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-10-25 10:53:57 +0000

    change the name of the class
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2325 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/PhoneCalls.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace Lync_Backend.Helpers
+{
+    public class PhoneCalls
+    {
+        public string SessionIdTime { set; get; }
+        public int SessionIdSeq { get; set; }
+        public string ResponseTime { set; get; }
+        public string SessionEndTime { set; get; }
+        public string SourceUserUri { set; get; }
+        public string SourceNumberUri { set; get; }
+        public string DestinationNumberUri { set; get; }
+        public string DestinationUserUri { get; set; }
+        public string FromMediationServer { set; get; }
+        public string ToMediationServer { set; get; }
+        public string FromGateway { set; get; }
+        public string ToGateway { set; get; }
+        public string SourceUserEdgeServer { set; get; }
+        public string DestinationUserEdgeServer { set; get; }
+        public string ServerFQDN { set; get; }
+        public string PoolFQDN { set; get; }
+        public string Marker_CallToCountry { set; get; }
+        
+        public string marker_CallType { set; get; }
+        public int Marker_CallTypeID { set; get; }
+        public long marker_CallFrom { set; get; }
+        public long marker_CallTo { set; get; }
+
+        public decimal Duration { set; get; }
+        public decimal Marker_CallCost { set; get; }
+
+        //User UI update Fields
+        public string UI_UpdatedByUser { set; get; }
+        public DateTime UI_MarkedOn { set; get; }
+        public string UI_CallType { set; get; }
+        public string AC_DisputeStatus { set; get; }
+        public DateTime AC_DisputeResolvedOn { set; get; }
+        public string AC_IsInvoiced { set; get; }
+        public DateTime AC_InvoiceDate { set; get; }
+
+        protected static List<NumberingPlan> numberingPlan = NumberingPlan.GetNumberingPlan();
+
+        protected static List<DIDs> dids = DIDs.GetDIDs();
+
+        protected static List<CallsTypes> callTypes = CallsTypes.GetCallTypes();
+
+        protected static List<string> ListOfUserUrisExceptions = PhoneCallsExceptions.GetUsersUris();
+        protected static List<string> ListOfUserNumbersExceptions = PhoneCallsExceptions.GetUsersNumbers();
+
+        protected bool MatchDID(string phoneNumber, out string site)
+        {
+            if (string.IsNullOrEmpty(phoneNumber)) 
+            {
+                site = string.Empty;
+                return false;
+            }
+
+            foreach (DIDs didEntry in dids)
+            {
+                string did = didEntry.did;
+
+                if (Regex.IsMatch(phoneNumber.Trim('+'), @"^"+did))
+                {
+                    site = didEntry.description;
+                    return true;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            site = string.Empty;
+            return false;
+        }
+
+        protected long GetDialingPrefixFromNumber(string phoneNumber, out string callType)
+        {
+            long numberToParse = 0;
+            
+            if (phoneNumber.Length >= 9 ) 
+            {
+                long.TryParse(phoneNumber, out numberToParse);
+
+                while (numberToParse > 0)
+                {
+                    var number = numberingPlan.Find(item => item.DialingPrefix == numberToParse);
+
+                    if (number != null)
+                    {
+
+                        callType = number.TypeOfService;
+                        return number.DialingPrefix;
+                    }
+                    else
+                    {
+                        numberToParse = numberToParse / 10;
+                        continue;
+                    }
+                }
+            }
+
+            callType = "N/A";
+          
+            long.TryParse(phoneNumber, out numberToParse);
+            return 0;
+        }
+
+        protected string FixNumberType(string number)
+        {
+
+            if (string.IsNullOrEmpty(number))
+                return "N/A";
+
+            if (number.Contains(";")) 
+            {
+                number = number.Split(';')[0].ToString();
+            }
+
+            number = number.Trim('+');
+
+
+            return number;
+        }
+
+    }
+}
