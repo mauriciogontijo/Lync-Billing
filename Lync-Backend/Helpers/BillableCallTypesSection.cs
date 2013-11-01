557bfab747db348bfa5a6586fc8c5d3a435e036d
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-11-01 09:36:56 +0000

    Create Configuration section for Billable Types
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2357 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/BillableCallTypesSection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Lync_Backend.Helpers
+{
+
+    public class BillableTypeElement : ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsKey=true, IsRequired=true)]
+        public string Name
+        {
+            get { return this["name"].ToString(); }
+        }
+
+        [ConfigurationProperty("value")]
+        public int Price
+        {
+            get { return Convert.ToInt32(this["value"]); }
+        }
+    }
+
+    public class BillableTypeCollection : ConfigurationElementCollection
+    {
+         protected override ConfigurationElement CreateNewElement()
+        {
+        return new BillableTypeElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+        return ((BillableTypeElement)element).Name;
+        }
+    }
+
+    public class BillableCallTypesSection : ConfigurationSection
+    {
+        [ConfigurationProperty("BillableTypes")]
+        public BillableTypeCollection BillableTypes
+        {
+            get { return (BillableTypeCollection)this["BillableTypes"]; }
+        }
+        
+    }
+}
