bc6bcd0313eae836f44ddf77632e7a04e4a2df0d
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-11-02 07:59:02 +0000

    Added the list of billable call types to the Web.Confing and have made a class for it.
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2369 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/ConfigurationSections/BillableCallTypesSection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Lync_Billing.ConfigurationSections
+{
+
+    public class BillableTypeElement : ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsKey=true, IsRequired=true)]
+        public string Name
+        {
+            get { return this["name"].ToString(); }
+        }
+
+        [ConfigurationProperty("value")]
+        public int Value
+        {
+            get { return Convert.ToInt32(this["value"]); }
+        }
+    }
+
+    public class BillableTypeCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new BillableTypeElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((BillableTypeElement)element).Name;
+        }
+    }
+
+    public class BillableCallTypesSection : ConfigurationSection
+    {
+        [ConfigurationProperty("BillableTypes")]
+        public BillableTypeCollection BillableTypes
+        {
+            get { return (BillableTypeCollection)this["BillableTypes"]; }
+        }
+
+
+        public List<int> BillableTypesList
+        {
+            get
+            {
+                List<int> billableTypesList = new List<int>() ;
+
+                foreach (BillableTypeElement el in BillableTypes)
+                {
+                    billableTypesList.Add(el.Value);
+                }
+
+                return billableTypesList;
+            }
+        }
+
+        //Easier to compare against agnostic keyValuePair with typeof functions
+        public Array BillableTypesArrayList
+        {
+            get
+            {
+                List<int> billableTypesList = new List<int>();
+
+                foreach (BillableTypeElement el in BillableTypes)
+                {
+                    billableTypesList.Add(el.Value);
+                }
+
+                return billableTypesList.ToArray();
+            }
+        }
+        
+    }
+}
