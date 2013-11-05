e822cd8e113831f72d7da67e77f4394ae2ccda10
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-11-05 07:03:37 +0000

    Call Marker and Rate Applier Classes which implements duartion based routines
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2398 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/CallMarker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lync_Backend.Helpers
+{
+    public class CallMarker
+    {
+        public static void MarkCalls(DateTime? optionalFrom = null, DateTime? optionalTo = null, string gateway = null)
+        {
+            DateTime from = optionalFrom != null ? optionalFrom.Value : DateTime.MinValue;
+            DateTime to = optionalTo != null ? optionalTo.Value : DateTime.MaxValue;
+
+            if (string.IsNullOrEmpty(gateway))
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
