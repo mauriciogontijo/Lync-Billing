2d52b62a373a5e50cf90ba2177c4a6808ea41ca2
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-11-08 14:22:57 +0000

    Finish Get_ChargeableCalls_ForUser
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2431 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Backend/Helpers/CreateDBStatistics.cs
+++ Lync-Backend/Helpers/CreateDBStatistics.cs
@@ -5,24 +5,94 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.OleDb;
 using System.Configuration;
+using Lync_Backend.Libs;
+using System.Reflection;
+
 
 
 namespace Lync_Backend.Helpers
 {
     class CreateDBStatistics
     {
-        private OleDbDataReader dataReader;
-        private OleDbConnection sourceDBConnector = new OleDbConnection(ConfigurationManager.ConnectionStrings["LyncConnectionString"].ConnectionString);
+        
+        private static OleDbConnection sourceDBConnector = new OleDbConnection(ConfigurationManager.ConnectionStrings["LyncConnectionString"].ConnectionString);
 
         //TODO: every Get_ChargeableCalls function should return a new column represents which table the phone call object is located 
        
         //STEP 1
         #region Chargeable Calls Functions
         
-        //High Priority
-        
+        private static void CreateOrAlterFunction(string functionName,string SQLStatement)
+        {
+            string functionCreateUpdateQuery = string.Empty;
+            string QueryType = string.Empty;
+
+            // SQL Statement to Check if the function already exisits in the Database or not and it will return empty string of not
+            string sqlValidationQuery = string.Format("select OBJECT_ID('{0}')", functionName);
+
+            using (sourceDBConnector)
+            {
+                OleDbCommand comm = new OleDbCommand(sqlValidationQuery, sourceDBConnector);
+
+                try
+                {
+                    sourceDBConnector.Open();
+
+                    string result = comm.ExecuteScalar().ToString();
+
+                    //Define the query Type
+                    if (!string.IsNullOrEmpty(result))
+                        QueryType = "ALTER";
+                    else
+                        QueryType = "CREATE";
+
+                    functionCreateUpdateQuery =
+                           string.Format("{0} FUNCTION [dbo].[{1}] (@SipAccount	nvarchar(450)) " +
+                                         "RETURNS TABLE AS RETURN ({2}) "
+                                         , QueryType, functionName, SQLStatement);
+
+                    comm.CommandText = functionCreateUpdateQuery;
+                    comm.ExecuteNonQuery();
+                }
+                catch (Exception ex) { }
+            }
+
+        }
+
         //Returns List of Chargeable Calls For A specific User
-        public static void Get_ChargeableCalls_ForUser() { }
+        public static void Get_ChargeableCalls_ForUser()
+        {
+
+            StringBuilder sqlStatement = new StringBuilder();
+
+            Dictionary<string, object> whereFieldsValues = new Dictionary<string, object>();
+            Dictionary<string, object> orderByFiledsValues = new Dictionary<string, object>();
+
+            Dictionary<string, MonitoringServersInfo> monInfo = MonitoringServersInfo.GetMonitoringServersInfo();
+
+            BillableCallTypesSection section = (BillableCallTypesSection)ConfigurationManager.GetSection("BillableCallTypesSection");
+
+            //convert BillableCallTypesIds to strings 1,2,3,4,5 ...etc
+            string BillableCallTypesIdsList = string.Join(",", section.BillableTypesList);
+
+            //Get WhereStatemnet and append it to every Select 
+            string whereStatement = 
+                string.Format(
+                    "WHERE " + 
+                    "[" +Enums.GetDescription(Enums.PhoneCalls.SourceUserUri) + "]=@SipAccount COLLATE SQL_Latin1_General_CP1_CI_AS AND " + 
+                    "[" + Enums.GetDescription(Enums.PhoneCalls.Marker_CallTypeID) + "] in ({0})"
+                    , BillableCallTypesIdsList);
+           
+            foreach (KeyValuePair<string, MonitoringServersInfo> keyValue in monInfo)
+            {
+                sqlStatement.Append(string.Format("SELECT * FROM [{0}] {1} UNION ", ((MonitoringServersInfo)keyValue.Value).PhoneCallsTable, whereStatement));
+            }
+
+            sqlStatement.Remove(sqlStatement.Length - 6, 5);
+
+            CreateOrAlterFunction(MethodBase.GetCurrentMethod().Name, sqlStatement.ToString());
+            
+        }
 
         //Returns List of Chargeable Calls For users in Specific Site
         public static void Get_ChargeableCalls_ForSite() { }
