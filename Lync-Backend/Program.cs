2772dba1cec51a703bef3f069a54b5a6ad976c9d
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-11-14 07:25:53 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2519 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Backend/Program.cs
+++ Lync-Backend/Program.cs
@@ -21,27 +21,54 @@ namespace Lync_Backend
 
             monServersInfo = MonitoringServersInfo.GetMonitoringServersInfo();
 
-            //foreach (KeyValuePair<string, MonitoringServersInfo> keyValue in monServersInfo)
-            //{
-            //    Type type = Type.GetType("Lync_Backend.Implementation." + keyValue.Key);
+            foreach (KeyValuePair<string, MonitoringServersInfo> keyValue in monServersInfo)
+            {
+                Type type = Type.GetType("Lync_Backend.Implementation." + keyValue.Key);
 
-            //    string fqdn = typeof(AbIdDatabaseImporter).AssemblyQualifiedName;
+                string fqdn = typeof(AbIdDatabaseImporter).AssemblyQualifiedName;
 
-            //    //FQN  for Lync2010: Lync_Backend.Implementation.Lync2010, Lync-Backend, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-            //    object instance = Activator.CreateInstance(type);
+                //FQN  for Lync2010: Lync_Backend.Implementation.Lync2010, Lync-Backend, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+                object instance = Activator.CreateInstance(type);
 
-            //    ((AbIdDatabaseImporter)instance).ImportGatewaysAndPools();
-            //    ((AbIdDatabaseImporter)instance).ImportPhoneCalls();
+                ((AbIdDatabaseImporter)instance).ImportGatewaysAndPools();
+                ((AbIdDatabaseImporter)instance).ImportPhoneCalls();
 
-            //    string tableName = ((Interfaces.AbIdDatabaseImporter)instance).PhoneCallsTableName;
+                string tableName = ((Interfaces.AbIdDatabaseImporter)instance).PhoneCallsTableName;
 
-            //    Interfaces.ICallMarker callsMarker = new CallMarker();
-            //    callsMarker.MarkCalls(tableName);
-            //}
+                //Interfaces.ICallMarker callsMarker = new CallMarker();
+                //callsMarker.MarkCalls(tableName);
+                //callsMarker.ApplyRates(tableName);
+
+            }
 
 
-            ICallMarker callsMarker = new CallMarker();
-            callsMarker.ApplyRates("PhoneCalls2010");
+            //CreateDBStatistics.Get_ChargeableCalls_ForUser();
+            //CreateDBStatistics.Get_ChargeableCalls_ForSite();
+
+            //CreateDBStatistics.Get_CallsSummary_ForUser();
+            //CreateDBStatistics.Get_CallsSummary_ForUsers_PerSite();
+            //CreateDBStatistics.Get_CallsSummary_ForUsers_PerSite_PDF();
+            //CreateDBStatistics.Get_CallsSummary_ForSiteDepartment();
+
+
+            //CreateDBStatistics.Get_DestinationCountries_ForUser();
+            //CreateDBStatistics.Get_DestinationCountries_ForSite();
+            //CreateDBStatistics.Get_DestinationCountries_ForSiteDepartment();
+
+            //CreateDBStatistics.Get_DestinationsNumbers_ForUser();
+            //CreateDBStatistics.Get_DestinationsNumbers_ForSite();
+            //CreateDBStatistics.Get_DestinationsNumbers_ForSiteDepartment();
+
+            //CreateDBStatistics.Get_GatewaySummary_PerUser();
+            //CreateDBStatistics.Get_GatewaySummary_PerSite();
+            //CreateDBStatistics.Get_GatewaySummary_PerSiteDepartment();
+            //CreateDBStatistics.Get_GatewaySummary_ForAll_Sites();
+            //CreateDBStatistics.Get_CallSummary_ForUsers_PerGateway();
+
+            //CreateDBStatistics.Get_MailStatistics_ForUsers_PerSiteDepartment();
+            //CreateDBStatistics.Get_MailStatistics_PerSiteDepartment();
+            //ICallMarker callsMarker = new CallMarker();
+            //callsMarker.ApplyRates("PhoneCalls2013");
             //callsMarker.ApplyRates("PhoneCalls2010");
 
         }
