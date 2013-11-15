f7a3386b80addddee95d9935a5d0ceeff4cc5add
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-11-15 11:09:55 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2539 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Backend/Interfaces/AbDatabaseMarker.cs
+++ Lync-Backend/Interfaces/AbDatabaseMarker.cs
@@ -11,15 +11,15 @@ namespace Lync_Backend.Interfaces
     {
 
         public abstract void MarkCalls(string tablename, DateTime? optionalFrom = null, DateTime? optionalTo = null, string gateway = null);
-        public abstract void MarkCalls(string tablename, ref PhoneCalls phoneCall);
+        public abstract void MarkCalls(string tableName, ref PhoneCalls phoneCall);
 
         public abstract void MarkExclusion(string tablename, DateTime? optionalFrom = null, DateTime? optionalTo = null, string gateway = null);
-        public abstract void MarkExclusion(string tablename,ref  PhoneCalls phoneCall);
+        public abstract void MarkExclusion(string tableName, ref  PhoneCalls phoneCall);
 
         public abstract void ApplyRates(string tablename, DateTime? optionalFrom = null, DateTime? optionalTo = null, string gateway = null);
-        public abstract void ApplyRates(string tablename,ref PhoneCalls phoneCall);
+        public abstract void ApplyRates(string tableName, ref PhoneCalls phoneCall);
 
         public abstract void ResetPhoneCallsAttributes(string tablename, DateTime? optionalFrom = null, DateTime? optionalTo = null, string gateway = null);
-        public abstract void ResetPhoneCallsAttributes(string tablename,ref  PhoneCalls phoneCall);
+        public abstract void ResetPhoneCallsAttributes(string tableName, ref  PhoneCalls phoneCall);
     }
 }
--- Lync-Backend/Interfaces/ICallMarker.cs
+++ Lync-Backend/Interfaces/ICallMarker.cs
@@ -10,15 +10,15 @@ namespace Lync_Backend.Interfaces
     interface ICallMarker
     {
         void MarkCalls(string tablename, DateTime? optionalFrom = null, DateTime? optionalTo = null, string gateway = null);
-        void MarkCalls(string tablename, ref PhoneCalls phoneCall);
+        void MarkCalls(ref PhoneCalls phoneCall);
         
         void MarkExclusion(string tablename, DateTime? optionalFrom = null, DateTime? optionalTo = null, string gateway = null);
-        void MarkExclusion(string tablename, ref PhoneCalls phoneCall);
+        void MarkExclusion(ref PhoneCalls phoneCall);
         
         void ApplyRates(string tablename, DateTime? optionalFrom = null, DateTime? optionalTo = null, string gateway = null);
-        void ApplyRates(string tablename, ref PhoneCalls phoneCall);
+        void ApplyRates(ref PhoneCalls phoneCall);
 
         void ResetPhoneCallsAttributes(string tablename, DateTime? optionalFrom = null, DateTime? optionalTo = null, string gateway = null);
-        void ResetPhoneCallsAttributes(string tablename,ref PhoneCalls phoneCall);
+        void ResetPhoneCallsAttributes(ref PhoneCalls phoneCall);
     }
 }
