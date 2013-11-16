174ea378b91830f9869e6983ed905cfc36466720
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-11-16 08:59:04 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2555 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Helpers/ObjectActivator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Linq.Expressions;
+
+
+namespace Lync_Backend.Helpers
+{
+    public class ObjectActivatorHelper
+    {
+
+        public delegate T ObjectActivator<T>(params object[] args);
+
+        public  static ObjectActivator<T> GetActivator<T> (ConstructorInfo ctor)
+        {
+            Type type = ctor.DeclaringType;
+            ParameterInfo[] paramsInfo = ctor.GetParameters();
+
+            //create a single param of type object[]
+            ParameterExpression param = Expression.Parameter(typeof(object[]), "args");
+
+            Expression[] argsExp = new Expression[paramsInfo.Length];
+
+            //pick each arg from the params array 
+            //and create a typed expression of them
+            for (int i = 0; i < paramsInfo.Length; i++)
+            {
+                Expression index = Expression.Constant(i);
+                Type paramType = paramsInfo[i].ParameterType;
+
+                Expression paramAccessorExp =
+                    Expression.ArrayIndex(param, index);
+
+                Expression paramCastExp =
+                    Expression.Convert(paramAccessorExp, paramType);
+
+                argsExp[i] = paramCastExp;
+            }
+
+            //make a NewExpression that calls the
+            //ctor with the args we just created
+            NewExpression newExp = Expression.New(ctor, argsExp);
+
+            //create a lambda with the New
+            //Expression as body and our param object[] as arg
+            LambdaExpression lambda =
+                Expression.Lambda(typeof(ObjectActivator<T>), newExp, param);
+
+            //compile it
+            ObjectActivator<T> compiled = (ObjectActivator<T>)lambda.Compile();
+            return compiled;
+        }
+    }
+
+    public class ActivatorsStorage
+    {
+        public delegate object ObjectActivator(params object[] args);
+
+        private readonly Dictionary<string, ObjectActivator> activators = new Dictionary<string, ObjectActivator>();
+
+        private ObjectActivator CreateActivator(ConstructorInfo ctor)
+        {
+            Type type = ctor.DeclaringType;
+
+            ParameterInfo[] paramsInfo = ctor.GetParameters();
+            ParameterExpression param = Expression.Parameter(typeof(object[]), "args");
+
+            Expression[] argsExp = new Expression[paramsInfo.Length];
+
+            for (int i = 0; i < paramsInfo.Length; i++)
+            {
+                Expression index = Expression.Constant(i);
+                Type paramType = paramsInfo[i].ParameterType;
+
+                Expression paramAccessorExp = Expression.ArrayIndex(param, index);
+
+                Expression paramCastExp = Expression.Convert(paramAccessorExp, paramType);
+
+                argsExp[i] = paramCastExp;
+            }
+
+            NewExpression newExp = Expression.New(ctor, argsExp);
+
+            LambdaExpression lambda = Expression.Lambda(typeof(ObjectActivator), newExp, param);
+
+            return (ObjectActivator)lambda.Compile();
+        }
+
+        private ObjectActivator CreateActivator(string className)
+        {
+            Type type = Type.GetType(className);
+            
+            if (type == null)
+                throw new ArgumentException("Incorrect class name", "className");
+
+            // Get contructor with one parameter
+            ConstructorInfo ctor = type.GetConstructors().SingleOrDefault(w => w.GetParameters().Length == 1  && w.GetParameters()[0].ParameterType == typeof(object));
+            //MethodInfo method =  type.GetMethod(methodName)(w => w.GetParameters().Length == 1  && w.GetParameters()[0].ParameterType == typeof(object));
+
+
+            if (ctor == null)
+                throw new Exception("There is no any constructor with 1 object parameter.");
+
+            return CreateActivator(ctor);
+        }
+
+        public ObjectActivator GetActivator(string className)
+        {
+            ObjectActivator activator;
+
+            if (activators.TryGetValue(className, out activator))
+            {
+                return activator;
+            }
+            activator = CreateActivator(className);
+            activators[className] = activator;
+            return activator;
+        }
+    }
+
+    public static class NewTpes<T> where T : new()
+    {
+        public static readonly Func<T> Instance = Expression.Lambda<Func<T>>( Expression.New(typeof(T)) ).Compile();
+    }
+
+   
+}
--- Lync-Backend/Program.cs
+++ Lync-Backend/Program.cs
@@ -9,14 +9,24 @@ using Lync_Backend.Libs;
 using Lync_Backend.Implementation;
 using Lync_Backend.Interfaces;
 using System.Configuration;
-using Lync_Backend.Interfaces;
 
-namespace Lync_Backend
+using System.Runtime.InteropServices;namespace Lync_Backend
 {
     class Program
     {
+        public const int SW_SHOWMINIMIZED = 2;
+        public const int SW_HIDE = 0;
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
         static void Main(string[] args)
         {
+            
+
+            IntPtr winHandle = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+            ShowWindow(winHandle, SW_HIDE);
+
             Dictionary<string, MonitoringServersInfo> monServersInfo = new Dictionary<string, MonitoringServersInfo>();
 
             monServersInfo = MonitoringServersInfo.GetMonitoringServersInfo();
@@ -30,7 +40,7 @@ namespace Lync_Backend
                 //FQN  for Lync2010: Lync_Backend.Implementation.Lync2010, Lync-Backend, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
                 object instance = Activator.CreateInstance(type);
 
-                //((AbIdDatabaseImporter)instance).ImportGatewaysAndPools();
+                ((AbIdDatabaseImporter)instance).ImportGatewaysAndPools();
                 ((AbIdDatabaseImporter)instance).ImportPhoneCalls();
 
                 //string tableName = ((Interfaces.AbIdDatabaseImporter)instance).PhoneCallsTableName;
@@ -47,33 +57,30 @@ namespace Lync_Backend
             //callsMarker.ApplyRates("PhoneCalls2010");
 
 
-            //CreateDBStatistics.Get_ChargeableCalls_ForUser();
-            //CreateDBStatistics.Get_ChargeableCalls_ForSite();
+            CreateDBStatistics.Get_ChargeableCalls_ForUser();
+            CreateDBStatistics.Get_ChargeableCalls_ForSite();
 
-            //CreateDBStatistics.Get_CallsSummary_ForUser();
-            //CreateDBStatistics.Get_CallsSummary_ForUsers_PerSite();
-            //CreateDBStatistics.Get_CallsSummary_ForUsers_PerSite_PDF();
-            //CreateDBStatistics.Get_CallsSummary_ForSiteDepartment();
+            CreateDBStatistics.Get_CallsSummary_ForUser();
+            CreateDBStatistics.Get_CallsSummary_ForUsers_PerSite();
+            CreateDBStatistics.Get_CallsSummary_ForUsers_PerSite_PDF();
+            CreateDBStatistics.Get_CallsSummary_ForSiteDepartment();
 
 
-            //CreateDBStatistics.Get_DestinationCountries_ForUser();
-            //CreateDBStatistics.Get_DestinationCountries_ForSite();
-            //CreateDBStatistics.Get_DestinationCountries_ForSiteDepartment();
+            CreateDBStatistics.Get_DestinationCountries_ForUser();
+            CreateDBStatistics.Get_DestinationCountries_ForSite();
+            CreateDBStatistics.Get_DestinationCountries_ForSiteDepartment();
 
-            //CreateDBStatistics.Get_DestinationsNumbers_ForUser();
-            //CreateDBStatistics.Get_DestinationsNumbers_ForSite();
-            //CreateDBStatistics.Get_DestinationsNumbers_ForSiteDepartment();
+            CreateDBStatistics.Get_DestinationsNumbers_ForUser();
+            CreateDBStatistics.Get_DestinationsNumbers_ForSite();
+            CreateDBStatistics.Get_DestinationsNumbers_ForSiteDepartment();
 
-            //CreateDBStatistics.Get_GatewaySummary_PerUser();
-            //CreateDBStatistics.Get_GatewaySummary_PerSite();
-            //CreateDBStatistics.Get_GatewaySummary_PerSiteDepartment();
-            //CreateDBStatistics.Get_GatewaySummary_ForAll_Sites();
+            CreateDBStatistics.Get_GatewaySummary_PerUser();
+            CreateDBStatistics.Get_GatewaySummary_PerSite();
+            CreateDBStatistics.Get_GatewaySummary_PerSiteDepartment();
+            CreateDBStatistics.Get_GatewaySummary_ForAll_Sites();
 
-            //CreateDBStatistics.Get_MailStatistics_ForUsers_PerSiteDepartment();
-            //CreateDBStatistics.Get_MailStatistics_PerSiteDepartment();
-            //ICallMarker callsMarker = new CallMarker();
-            //callsMarker.ApplyRates("PhoneCalls2013");
-            //callsMarker.ApplyRates("PhoneCalls2010");
+            CreateDBStatistics.Get_MailStatistics_ForUsers_PerSiteDepartment();
+            CreateDBStatistics.Get_MailStatistics_PerSiteDepartment();
 
         }
     }
