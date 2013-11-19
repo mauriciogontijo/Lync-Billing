6c4cb896b02d55ea312f07292add3265f49970f1
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-11-19 11:16:44 +0000

    Add Calls Exceptions Class
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2592 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Backend/Implementation/CallsExceptions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lync_Backend.Helpers;
+using Lync_Backend.Libs;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace Lync_Backend.Implementation
+{
+    public class CallsExceptions
+    {
+        private DBLib DBRoutines = new DBLib();
+
+        public static void ApplyMOAExceptions(ref PhoneCalls phoneCall,string siteName) 
+        {
+
+            if (siteName == "MOA")
+            {
+                //Check if call has been made from moa to greek land line
+                if(phoneCall.DestinationNumberUri.StartsWith("+302"))
+                    phoneCall.Marker_CallCost = Convert.ToDecimal(0);
+
+                OleDbConnection sourceDBConnector = new OleDbConnection(ConfigurationManager.ConnectionStrings["LyncConnectionString"].ConnectionString
+                
+                string sqlValidationQuery = string.Format("SELECT Number FROM PhoneCallsExceptions WHERE Number='{0}'", phoneCall.DestinationNumberUri);
+
+                OleDbCommand comm = new OleDbCommand(sqlValidationQuery, sourceDBConnector);
+
+                try
+                {
+                    sourceDBConnector.Open();
+
+                    string result = comm.ExecuteScalar().ToString();
+
+                    if (result == phoneCall.DestinationNumberUri) 
+                    {
+                        phoneCall.Marker_CallCost = Convert.ToDecimal(0);
+                    }
+
+                }
+                catch (Exception ex){ }
+                
+                finally
+                {
+                    sourceDBConnector.Close();
+                }
+
+            }
+        }
+
+    }
+}
