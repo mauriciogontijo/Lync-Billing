541a44f40d55e9112fc907225078e925616d1f3b
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-11-21 12:39:07 +0000

    Restructured the pages into two distinct directories
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2672 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/UI/delegee/site/phonecalls.aspx.cs
@@ -0,0 +1,45 @@
+﻿using Ext.Net;
+using Lync_Billing.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lync_Billing.ui.delegee.site
+{
+    public partial class phonecalls : System.Web.UI.Page
+    {
+        private UserSession session;
+        private string sipAccount = string.Empty;
+        private string delegatedSiteName = string.Empty;
+        private string allowedRoleName = Enums.GetDescription(Enums.ActiveRoleNames.SiteDelegee);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //If the user is not loggedin, redirect to Login page.
+            if (HttpContext.Current.Session == null || HttpContext.Current.Session.Contents["UserData"] == null)
+            {
+                //Don't enable the redirection process because the site name will be missing!
+                //string redirect_to = @"~/ui/delegee/site.aspx";
+                //string url = @"~/ui/session/login.aspx?redirect_to=" + redirect_to;
+
+                string url = @"~/ui/session/login.aspx";
+                Response.Redirect(url);
+            }
+            else
+            {
+                session = (UserSession)HttpContext.Current.Session.Contents["UserData"];
+
+                if (session.ActiveRoleName != allowedRoleName)
+                {
+                    Response.Redirect("~/ui/session/authenticate.aspx?access=" + allowedRoleName);
+                }
+            }
+
+            sipAccount = session.EffectiveSipAccount;
+            delegatedSiteName = session.EffectiveDelegatedSiteName;
+        }
+    }
+}
