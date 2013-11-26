55e5333c1886b79d44cbed98b90bc815c43440a1
aalhour <aalhour@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-11-26 09:20:39 +0000

    Added the PhoneCallsPerPage JSON string to the DelegeeAccount class.
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2727 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Billing/DB/DelegeeAccountInfo.cs
+++ Lync-Billing/DB/DelegeeAccountInfo.cs
@@ -9,13 +9,17 @@ namespace Lync_Billing.DB
     {
         public int DelegeeTypeID { get; set; }
 
+        //Site Delegate Role related
         public Site DelegeeSiteAccount { get; set; }
 
+        //Departent Delegate Role related
         public Department DelegeeDepartmentAccount { get; set; }
 
+        //User Delegate Role related
         public Users DelegeeUserAccount { get; set; }
         public string DelegeeUserPhonecallsPerPage { set; get; }
         public List<PhoneCall> DelegeeUserPhonecalls { get; set; }
+        public List<PhoneCall> DelegeeUserPhonecallsHistory { get; set; }
         public Dictionary<string, PhoneBook> DelegeeUserAddressbook { set; get; }
     }
 }
