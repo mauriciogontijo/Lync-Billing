63e1bf3504e01d88a154da10965c72ef245dcebd
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-11-26 08:37:45 +0000

    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2724 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

--- Lync-Backend/Helpers/SQLs.cs
+++ Lync-Backend/Helpers/SQLs.cs
@@ -110,47 +110,72 @@ namespace Lync_Backend.Helpers
             string SELECT_STATEMENT = string.Empty;
             string ORDER_BY = string.Empty;
 
-            SELECT_STATEMENT = String.Format(
-               "SELECT " +
-                        "VoipDetails.SessionIdTime, " +
-                        "VoipDetails.SessionIdSeq, " +
-                        "Users_1.UserUri AS SourceUserUri, " +
-                        "Users_2.UserUri AS DestinationUserUri, " +
-                        "Phones.PhoneUri AS SourceNumberUri, " +
-                        "Phones_1.PhoneUri AS DestinationNumberUri, " +
-                        "MediationServers.MediationServer AS FromMediationServer, " +
-                        "MediationServers_1.MediationServer AS ToMediationServer, " +
-                        "Gateways.Gateway AS FromGateway, " +
-                        "Gateways_1.Gateway AS ToGateway, " +
-                        "EdgeServers.EdgeServer AS SourceUserEdgeServer, " +
-                        "EdgeServers_1.EdgeServer AS DestinationUserEdgeServer, " +
-                        "Servers.ServerFQDN, " +
-                        "Pools.PoolFQDN, " +
-                        "SessionDetails.ResponseTime, " +
-                        "SessionDetails.SessionEndTime, " +
-                        "OnBehalf.UserUri AS OnBehalf," +
-		                "ReferredBy.UserUri AS ReferredBy," +
-                        "CONVERT(decimal(8, 0), " +
-                        "DATEDIFF(second, SessionDetails.ResponseTime,  SessionDetails.SessionEndTime)) AS Duration " +
-               "FROM     SessionDetails " +
-                        "LEFT OUTER JOIN Servers ON SessionDetails.ServerId = Servers.ServerId " +
-                        "LEFT OUTER JOIN Pools ON SessionDetails.PoolId = Pools.PoolId " +
-                        "LEFT OUTER JOIN SIPResponseMetaData ON SessionDetails.ResponseCode = SIPResponseMetaData.ResponseCode " +
-                        "LEFT OUTER JOIN EdgeServers AS EdgeServers_1 ON SessionDetails.User2EdgeServerId = EdgeServers_1.EdgeServerId " +
-                        "LEFT OUTER JOIN EdgeServers ON SessionDetails.User1EdgeServerId = EdgeServers.EdgeServerId " +
-                        "LEFT OUTER JOIN Users AS Users_2 ON SessionDetails.User2Id = Users_2.UserId " +
-                        "LEFT OUTER JOIN Users AS Users_1 ON SessionDetails.User1Id = Users_1.UserId " +
-                        "LEFT OUTER JOIN Users AS OnBehalf ON SessionDetails.OnBehalfOfId = OnBehalf.UserId " +
-                        "LEFT OUTER JOIN Users AS ReferredBy ON SessionDetails.ReferredById = ReferredBy.UserId " +
-                        "RIGHT OUTER JOIN VoipDetails " +
-                        "LEFT OUTER JOIN Gateways AS Gateways_1 ON VoipDetails.ToGatewayId = Gateways_1.GatewayId " +
-                        "LEFT OUTER JOIN Gateways ON VoipDetails.FromGatewayId = Gateways.GatewayId " +
-                        "LEFT OUTER JOIN MediationServers AS MediationServers_1 ON VoipDetails.ToMediationServerId = MediationServers_1.MediationServerId " +
-                        "LEFT OUTER JOIN MediationServers ON VoipDetails.FromMediationServerId = MediationServers.MediationServerId " +
-                        "LEFT OUTER JOIN Phones AS Phones_1 ON VoipDetails.ConnectedNumberId = Phones_1.PhoneId " +
-                        "LEFT OUTER JOIN Phones ON VoipDetails.FromNumberId = Phones.PhoneId ON SessionDetails.SessionIdTime = VoipDetails.SessionIdTime AND " +
-                        "SessionDetails.SessionIdSeq = VoipDetails.SessionIdSeq "
-           );
+            SELECT_STATEMENT = String.Format
+            (
+                "SELECT  " +
+                    "VoipDetails.SessionIdTime,   " +
+                    "VoipDetails.SessionIdSeq, " +
+                    "Users_1.UserUri AS SourceUserUri,  " +
+                    "Phones_1.PhoneUri AS SourceNumberUri,   " +
+                    "Users_2.UserUri AS DestinationUserUri,   " +
+                    "Phones_2.PhoneUri AS DestinationNumberUri,   " +
+                    "REPLACE(UserCalleeURI.URI,'sip:','') AS CalleeURI, " +
+                    "MediationServers.MediationServer AS FromMediationServer,   " +
+                    "MediationServers_1.MediationServer AS ToMediationServer,   " +
+                    "Gateways.Gateway AS FromGateway,   " +
+                    "Gateways_1.Gateway AS ToGateway,   " +
+                    "EdgeServers.EdgeServer AS SourceUserEdgeServer,   " +
+                    "EdgeServers_1.EdgeServer AS DestinationUserEdgeServer,   " +
+                    "Servers.ServerFQDN,   " +
+                    "Pools.PoolFQDN,   " +
+                    "SessionDetails.ResponseTime,   " +
+                    "SessionDetails.SessionEndTime,   " +
+                    "OnBehalf.UserUri AS OnBehalf,  " +
+                    "ReferredBy.UserUri AS ReferredBy, " +
+                    "CONVERT(decimal(8, 0), DATEDIFF(second, SessionDetails.ResponseTime,  SessionDetails.SessionEndTime)) AS Duration    " +
+                "FROM DialogsView " +
+                "RIGHT OUTER JOIN VoipDetails as VoipDetails on  " +
+                    "VoipDetails.SessionIdTime = DialogsView.LsCDRSessionIdTime and  " +
+                    "VoipDetails.SessionIdSeq = DialogsView.LsCDRSessionIdSeq " +
+                "LEFT OUTER JOIN SessionDetails as SessionDetails on  " +
+                    "SessionDetails.SessionIdTime = DialogsView.LsCDRSessionIdTime and  " +
+                    "VoipDetails.SessionIdSeq = DialogsView.LsCDRSessionIdSeq " +
+                "LEFT OUTER JOIN [QoEMetrics].[dbo].[Session] as QoEMetricsSession on  " +
+                    "QoEMetricsSession.ConferenceDateTime =  DialogsView.QoEMetricsSessionIdTime and  " +
+                    "QoEMetricsSession.SessionSeq =  DialogsView.QoEMetricsSessionIdSeq " +
+                "LEFT OUTER JOIN Servers ON  " +
+                    "SessionDetails.ServerId = Servers.ServerId   " +
+                "LEFT OUTER JOIN Pools ON  " +
+                    "SessionDetails.PoolId = Pools.PoolId   " +
+                "LEFT OUTER JOIN SIPResponseMetaData ON  " +
+                    "SessionDetails.ResponseCode = SIPResponseMetaData.ResponseCode   " +
+                "LEFT OUTER JOIN EdgeServers AS EdgeServers_1 ON  " +
+                    "SessionDetails.User2EdgeServerId = EdgeServers_1.EdgeServerId   " +
+                "LEFT OUTER JOIN EdgeServers ON  " +
+                    "SessionDetails.User1EdgeServerId = EdgeServers.EdgeServerId   " +
+                "LEFT OUTER JOIN Users AS Users_2 ON  " +
+                    "SessionDetails.User2Id = Users_2.UserId   " +
+                "LEFT OUTER JOIN Users AS Users_1 ON  " +
+                    "SessionDetails.User1Id = Users_1.UserId   " +
+                "LEFT OUTER JOIN Users AS OnBehalf ON  " +
+                    "SessionDetails.OnBehalfOfId = OnBehalf.UserId    " +
+                "LEFT OUTER JOIN Users AS ReferredBy ON   " +
+                    "SessionDetails.ReferredById = ReferredBy.UserId  " +
+                "LEFT OUTER JOIN Gateways AS Gateways_1 ON  " +
+                    "VoipDetails.ToGatewayId = Gateways_1.GatewayId   " +
+                "LEFT OUTER JOIN Gateways ON  " +
+                    "VoipDetails.FromGatewayId = Gateways.GatewayId   " +
+                "LEFT OUTER JOIN MediationServers AS MediationServers_1 ON  " +
+                    "VoipDetails.ToMediationServerId = MediationServers_1.MediationServerId   " +
+                "LEFT OUTER JOIN MediationServers ON  " +
+                    "VoipDetails.FromMediationServerId = MediationServers.MediationServerId   " +
+                "LEFT OUTER JOIN Phones AS Phones_1 ON  " +
+                    "VoipDetails.FromNumberId = Phones_1.PhoneId    " +
+                "LEFT OUTER JOIN Phones AS Phones_2 ON  " +
+                    "VoipDetails.ConnectedNumberId = Phones_2.PhoneId   " +
+                "LEFT OUTER JOIN [QoEMetrics].[dbo].[User] AS UserCalleeURI on  " +
+                    "QoEMetricsSession.CalleeURI = UserCalleeURI.UserKey "
+            );
 
             if (LAST_IMPORTED_PHONECALL_DATE != null)
             {
