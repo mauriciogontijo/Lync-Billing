c9eba0aa0a0295864ab7b3138c7665b184082fa8
sghaida <sghaida@49dcb018-ab0e-11e2-9a69-a5c50cd020d1>
2013-12-28 07:59:14 +0000

    Implement GetDepartmentsInSite
    git-svn-id: svn://10.1.46.26/lync-billing/Lync-Billing@2909 49dcb018-ab0e-11e2-9a69-a5c50cd020d1

---
+++ Lync-Billing/Backend/SitesDepartments.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using Lync_Billing.Libs;
+
+namespace Lync_Billing.Backend
+{
+    public class SitesDepartments
+    {
+        public int ID { get; set; }
+        public int SiteID { get; set; }
+        public int DepartmentID { get; set; }
+
+        private static DBLib DBRoutines = new DBLib();
+
+        public static List<SitesDepartments> GetDepartmentsInSite(int siteID)
+        {
+            DataTable dt;
+            List<string> columns;
+            Dictionary<string, object> wherePart;
+
+            SitesDepartments department;
+
+            List<SitesDepartments> departmentsList = new List<SitesDepartments>();
+
+            columns = new List<string>();
+            wherePart = new Dictionary<string, object>
+            { 
+                { Enums.GetDescription(Enums.SitesDepartmnets.SiteID), siteID } 
+            };
+
+            dt = DBRoutines.SELECT(Enums.GetDescription(Enums.SitesDepartmnets.TableName), columns, wherePart, 0);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                department = new SitesDepartments();
+
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName == Enums.GetDescription(Enums.SitesDepartmnets.ID))
+                        department.DepartmentID = Convert.ToInt32(HelperFunctions.ReturnZeroIfNull(row[column.ColumnName]));
+
+                    else if (column.ColumnName == Enums.GetDescription(Enums.SitesDepartmnets.SiteID))
+                    {
+                        department.SiteID = Convert.ToInt32(HelperFunctions.ReturnZeroIfNull(row[column.ColumnName]));
+                    }
+
+                    else if (column.ColumnName == Enums.GetDescription(Enums.SitesDepartmnets.DepartmentID))
+                    {
+                        department.DepartmentID = Convert.ToInt32(HelperFunctions.ReturnZeroIfNull(row[column.ColumnName]));
+                       
+                    }
+                }
+
+                departmentsList.Add(department);
+            }
+
+            return departmentsList;
+        }
+
+    }
+}
